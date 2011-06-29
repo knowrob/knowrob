490e853a7c0cb59519b6292e8e488656f28dd4d2
kunzel <kunzel@63041b90-49fb-4ca4-89d8-2ccc525231da>
2011-06-29 11:05:54 +0000

    added some search predicates for objects (WIP)
    git-svn-id: http://code.in.tum.de/svn/knowrob/trunk@333 63041b90-49fb-4ca4-89d8-2ccc525231da

--- comp_semantic_map/prolog/comp_semantic_map.pl
+++ comp_semantic_map/prolog/comp_semantic_map.pl
@@ -1,6 +1,6 @@
 /** <module> comp_semantic_map
 
-This module contains all computables that calculate relations for a semantic map.
+This module contains computables related to a semantic map.
 
 Copyright (c) 2011, Lars Kunze <kunzel@cs.tum.edu>
 All rights reserved.
@@ -36,11 +36,26 @@ POSSIBILITY OF SUCH DAMAGE.
 
 :- module(comp_semantic_map,
     [
-     
      comp_number_of_levels/2,
      comp_number_of_stories/2,
      comp_has_rooms/2,
-     nth_level_of_building/3
+     nth_level_of_building/3,
+     
+     locOfObj/2,
+     locOfObjT/2,
+     locOfSimilarObjT/2,
+     locOfMostSimilarObjT/2,
+     locOfKMostSimilarObjT/3,
+     locOfSimilarObjTGtWup/3,
+     locOfSimilarObjTBecause/3,
+     locOfMostSimilarObjTBecause/3,
+     locOfKMostSimilarObjTBecause/4,
+     locOfSimilarObjTGtWupBecause/4,
+
+     searchFoodOrDrinkTypeAtLocation/2,
+     searchFoodOrDrinkTypeAtLocationType/2,
+     searchFoodOrDrinkTypeInStorageConstruct/2,
+     searchFoodOrDrinkTypeAtServiceLocation/2
     ]).
 
 %%  hasRooms(L|B,R), hasLevels(B,L), objLoc(O,L), objLocWrtMap(O,L,M)
@@ -56,13 +71,32 @@ POSSIBILITY OF SUCH DAMAGE.
 :- rdf_db:rdf_register_ns(comp_sem_map, 'http://ias.cs.tum.edu/kb/comp_semantic_map.owl#', [keep(true)]).
 
 
-
 % define predicates as rdf_meta predicates
 % (i.e. rdf namespaces are automatically expanded)
 :-  rdf_meta
   comp_number_of_levels(r,-),
   comp_number_of_stories(r,-),
-  nth_level_of_building(@,r,r).
+  nth_level_of_building(@,r,r),
+  
+  searchFoodOrDrinkTypeAtLocation(r,r),
+  searchFoodOrDrinkTypeAtLocationType(r,r),
+  searchFoodOrDrinkTypeInStorageConstruct(r,r),
+  searchFoodOrDrinkTypeAtServiceLocation(r,r),
+
+  locOfObj(r, r),
+
+  locOfObjT(r, r),
+  
+  locOfSimilarObjT(r, r),
+  locOfMostSimilarObjT(r, r),
+  locOfKMostSimilarObjT(r, -, r),
+  locOfSimilarObjTGtWup(r, -, r),
+
+  locOfSimilarObjTBecause(r, r, r),
+  locOfMostSimilarObjTBecause(r, r, r),
+  locOfKMostSimilarObjTBecause(r, -, r, r),
+  locOfSimilarObjTGtWupBecause(r, -, r, r).
+
 
 
 comp_number_of_levels(B, N):-
@@ -86,4 +120,162 @@ nth_level_of_building(Number, Building, Level) :-
   owl_has(Building, knowrob:hasLevels, Level), 
   owl_has(Level, knowrob:floorNumber, literal(N)),
   atom_number(N, Number).
+
+% todo add computable
+%comp_in_room(_O, _R).
+%comp_in_level(_O, _L).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+locOfObj(Obj, Loc):-
+  owl_individual_of(Obj, knowrob:'SpatialThing-Localized'),
+  rdf_triple(knowrob:orientation, Obj, Loc).
+
+% computable wrappers for all predicates below obj -> objT? 
+
+locOfObjT(ObjT, Loc):-
+  owl_subclass_of(ObjT, knowrob:'SpatialThing-Localized'),
+  owl_has(Obj, rdf:type, ObjT),
+  rdf_triple(knowrob:orientation, Obj, Loc).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% locations of objects with similar type
+% object is known or unknown
+% if unknown look at locations of similar object types (using WUP)
+
+
+locOfSimilarObjT(ObjT, Loc):-
+  locOfSimilarObjTBecause(ObjT, Loc, _).
+
+locOfSimilarObjTBecause(ObjT, Loc, [S,T]):-
+  similarObjTypes(ObjT,SimTypes),
+  member([S,T], SimTypes),
+  locOfObjT(T,Loc).
+
+locOfMostSimilarObjT(ObjT, Loc):-
+  locOfMostSimilarObjTBecause(ObjT, Loc, _).
+
+locOfMostSimilarObjTBecause(ObjT, Loc, Because):-
+  locOfKMostSimilarObjTBecause(ObjT, 1, Loc, Because).
+
+locOfKMostSimilarObjT(ObjT, K, Loc):-
+  locOfKMostSimilarObjTBecause(ObjT, K, Loc, _).
+  
+locOfKMostSimilarObjTBecause(ObjT, K, Loc, [S, T]):-
+  similarObjTypes(ObjT,SimTypes),
+  nth0(N, SimTypes, [S,T]),
+  N < K,
+  locOfObjT(T,Loc).
+
+locOfSimilarObjTGtWup(ObjT, Wup, Loc):-
+  locOfSimilarObjTGtWupBecause(ObjT, Wup, Loc, _).
+
+locOfSimilarObjTGtWupBecause(ObjT, Wup, Loc, [S,T]):-
+  similarObjTypes(ObjT,SimTypes),
+  member([S,T], SimTypes),
+  S >= Wup,
+  locOfObjT(T,Loc).
+  
+similarObjTypes(ObjT, DecreasingSimTypes):- 
+  % find all located instances (with orientation)
+  % in the map and compare their types with ObjT
+  % return sorted list of [Similarity, Type] tuples
+  findall(T, (locOfObj(O,_L), owl_has(O, rdf:type, T)), TypesL),
+  list_to_set(TypesL, TypesS),
+  findall([Sim,T], (member(T,TypesS), rdf_wup_similarity(ObjT, T, Sim)), SimTypes),
+  sort(SimTypes, IncreasingSimTypes),
+  reverse(IncreasingSimTypes, DecreasingSimTypes).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% searchSpaceForObj(Obj, Loc):-
+%   owl_individual_of(Obj, knowrob:'SpatialThing-Localized'),
+%   owl_has(Obj, rdf:type, ObjT),
+%   searchSpaceForObjT(ObjT,Loc).
+
+% searchSpaceForObjT(ObjT, Loc):-
+%   searchSpaceForObjTBecauseEvtT(ObjT, _, _, Loc).
+
+% look at places where instances of this objT are created.
+locOfCreationEvtOfObjT(ObjT, Loc):-
+  locOfCreationEvtOfObjTBecause(ObjT, _, _, Loc).
+
+locOfCreationEvtOfObjTBecause(ObjT, EvtT, LocT, Loc):-
+  createdAtLocationTBecause(ObjT, LocT, EvtT),
+  locOfObjT(LocT,Loc).
+
+createdAtLocationTBecause(ObjT, LocT, EvtT) :-
+  createdInEventT(ObjT, EvtT),
+  findall(SubL, (owl_restriction_on(EvtT, restriction(knowrob:eventOccursAt, some_values_from(L))), owl_subclass_of(SubL,L) ), Locations),
+  list_to_set(Locations, LocationsSet),
+  member(LocT, LocationsSet).
+
+createdInEventT(ObjT, EvtT) :-
+  owl_subclass_of(ObjT, knowrob:'SpatialThing-Localized'),
+  owl_subclass_of(EvtT, knowrob:'ActionOnObject'),
+  owl_restriction_on(EvtT, restriction(knowrob:'outputsCreated', some_values_from(ObjT))).
+
+
+% prepareFoodAt(Obj, LocT) :-
+%   foodCreatedAtLocationType(Obj, LocT),
+%   owl_subclass_of(R, knowrob:'Kitchen').
+% all ingredients and tools available
+% have  capability to prepare item
+
+% not working as is
+% servedAtLocBy(ObjT,MAT,Res):-
+%                                 %  createdInRestaurant(Obj, Res, EvT):-
+%   owl_subclass_of(ObjT, knowrob:'FoodOrDrink'),
+%   createdAtLocationTBecause(ObjT, LocT, _EvT),
+%                                 % look for food in a restaurant
+%   owl_subclass_of(R, knowrob:'Restaurant-Organization'),
+%   owl_restriction_on(R, restriction(knowrob:servesCuisineAtLocation, some_values_from(LocT))),
+%   owl_individual_of(Loc,LoT),
+%   locOfObj(Loc,MAT),
+%   owl_restriction_on(R, restriction(knowrob:servesCuisine, some_values_from(CuisineT))),
+%   owl_restriction_on(ObjT, restriction(knowrob:commonFoodTypeOfCuisine, some_values_from(CuisineT))),
+%   findall(Ri, owl_individual_of(Ri, R), Rs),
+%   list_to_set(Rs,RsSet),
+%   member(Res, RsSet).
+  
+
+
+locOfStoragePlaceForObjT(ObjT, Loc):-
+  locOfStoragePlaceForObjTBecause(ObjT, _, _ ,Loc).
+
+locOfStoragePlaceForObjTBecause(ObjT, SPForType, LocT, Loc):-
+  owl_subclass_of(LocT,    knowrob:'StorageConstruct'),
+  owl_restriction_on(LocT, restriction(knowrob:'typePrimaryFunction-StoragePlaceFor', some_values_from(SPForType))),
+  owl_subclass_of(ObjT, SPForType),
+  locOfObjT(LocT, Loc).
+
+
+
+searchFoodOrDrinkTypeAtLocation(FoodOrDrink, Location) :-
+  searchFoodOrDrinkTypeAtLocationType(FoodOrDrink, LocationT),
+  owl_individual_of(Location, LocationT).
+  
+
+searchFoodOrDrinkTypeAtLocationType(FoodOrDrinkT, LocationT) :-
+  ( searchFoodOrDrinkTypeInStorageConstruct(FoodOrDrinkT, LocationT) ;
+    searchFoodOrDrinkTypeAtServiceLocation(FoodOrDrinkT, LocationT)).
+
+searchFoodOrDrinkTypeInStorageConstruct(FoodOrDrinkT, LocationT) :-
+  owl_subclass_of(FoodOrDrinkT, knowrob:'FoodOrDrink'),
+  owl_subclass_of(LocationT,    knowrob:'StorageConstruct'),
+  owl_restriction_on(LocationT, restriction(knowrob:'typePrimaryFunction-StoragePlaceFor', some_values_from(ObjT))),
+  owl_subclass_of(FoodOrDrinkT, ObjT).
+
+searchFoodOrDrinkTypeAtServiceLocation(FoodOrDrinkT, LocationT) :-
+  owl_subclass_of(FoodOrDrinkT, knowrob:'FoodOrDrink'),
+  owl_restriction_on(FoodOrDrinkT, restriction(knowrob:'commonFoodTypeOfCuisine', some_values_from(CuisineT))),
+  owl_subclass_of(FoodAndBeverageOrganizationT, knowrob:'FoodAndBeverageOrganization'),
+  owl_restriction_on(FoodAndBeverageOrganizationT, restriction(knowrob:'servesCuisine', some_values_from(CuisineT))),
+  owl_subclass_of(LocationT, knowrob:'MultiRoomUnit'),
+  owl_restriction_on(FoodAndBeverageOrganizationT, restriction(knowrob:'servesCuisineAtLocation', some_values_from(LocationT))).
+  
 
