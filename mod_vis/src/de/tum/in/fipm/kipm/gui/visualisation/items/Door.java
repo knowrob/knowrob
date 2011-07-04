f73f39ac486ccd1456c66b95e48bfda35a078bc1
tenorth <tenorth@63041b90-49fb-4ca4-89d8-2ccc525231da>
2011-07-04 16:37:01 +0000

    added hinge and door visualization models
    git-svn-id: http://code.in.tum.de/svn/knowrob/trunk@361 63041b90-49fb-4ca4-89d8-2ccc525231da

--- mod_vis/src/de/tum/in/fipm/kipm/gui/visualisation/items/Door.java
+++ mod_vis/src/de/tum/in/fipm/kipm/gui/visualisation/items/Door.java
@@ -5,9 +5,9 @@ import javax.vecmath.Vector3d;
 
 import edu.tum.cs.vis.Canvas;
 
-public class Box extends Item {
+public class Door extends Item {
 
-	public Box(float m00, float m01, float m02, float m03, float m10,
+	public Door(float m00, float m01, float m02, float m03, float m10,
 			float m11, float m12, float m13, float m20, float m21, float m22,
 			float m23, float m30, float m31, float m32, float m33, float xdim,
 			float ydim, float zdim) {
@@ -15,8 +15,8 @@ public class Box extends Item {
 		super(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31,
 				m32, m33, xdim, ydim, zdim);
 	}
-
-	public Box(Matrix4d pose, Vector3d dim){
+	
+	public Door(Matrix4d pose, Vector3d dim){
 		super(pose, dim);
 	}
 	
