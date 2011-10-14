c41ba17f1e7e0076e8a0c5573b5affc0ef50f66d
profanter <profanter@63041b90-49fb-4ca4-89d8-2ccc525231da>
2011-10-14 15:17:55 +0000

    first test with 3D Model
    git-svn-id: http://code.in.tum.de/svn/knowrob/trunk@454 63041b90-49fb-4ca4-89d8-2ccc525231da

--- mod_vis/src/de/tum/in/fipm/kipm/gui/visualisation/items/Bottle.java
+++ mod_vis/src/de/tum/in/fipm/kipm/gui/visualisation/items/Bottle.java
@@ -4,11 +4,11 @@ import javax.vecmath.Matrix4d;
 import javax.vecmath.Vector3d;
 
 import edu.tum.cs.vis.Canvas;
-import edu.tum.cs.util.math.Vector3f;
-import edu.tum.cs.vis.items.Cylinder;
+import edu.tum.cs.vis.model.parser.ColladaParser;
 
 public class Bottle extends Item {
 	
+	private ColladaParser parser;
 
 	public Bottle(float m00, float m01, float m02, float m03, float m10,
 			float m11, float m12, float m13, float m20, float m21, float m22,
@@ -16,17 +16,45 @@ public class Bottle extends Item {
 			float ydim, float zdim) {
 		super(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31,
 				m32, m33, xdim, ydim, zdim);
+		parser = ColladaParser.loadModel(colladaFile, "/home/stefan/");
 	}
 
 	public Bottle(Matrix4d pose, Vector3d dim){
 		super(pose, dim);
+		parser = ColladaParser.loadModel(colladaFile, "/home/stefan/");
 	}
 	
-	@Override
+	/*@Override
 	public void drawIt(Canvas c) {
 		
 		(new Cylinder(new Vector3f(0f, 0f, 0f), new Vector3f(0f, 0f, 15f), 0.04f)).draw(c); // cup
 		(new Cylinder(new Vector3f(0f, 0f, 15f), new Vector3f(0f, 0f, 25f), 0.015f)).draw(c); // nozzle
 		
+	}*/
+
+	
+	/*
+	 * Modified for testing with collada
+	 */
+	
+	private static String colladaFile = "/home/stefan/Coke2.dae";
+	
+	@Override
+	public void drawIt(Canvas c) {
+		parser.draw(c);
+		c.noFill();
+		c.strokeWeight(1);
+		c.stroke(204, 120, 15);
+		parser.drawBoundingBox(c);
+
+		/*
+		 * ColladaLoader colladaLoader =
+		 * ColladaLoader.getInstance(colladaFile,c); colladaLoader.scale(3.0f);
+		 * c.translate(0, 0); //goodies only for a better view
+		 * colladaLoader.draw();
+		 */
+		
+		//c.box(this.xdim, this.ydim, this.zdim);
+
 	}
 }
