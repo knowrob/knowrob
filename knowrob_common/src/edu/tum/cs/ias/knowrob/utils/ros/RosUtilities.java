4b8d35978a29c270ef4029f689fed21a78cb425a
tenorth <tenorth@63041b90-49fb-4ca4-89d8-2ccc525231da>
2011-11-17 16:58:48 +0000

    start and kill processes from KnowRob via rosrun (e.g. map_servers)
    git-svn-id: http://code.in.tum.de/svn/knowrob/trunk@519 63041b90-49fb-4ca4-89d8-2ccc525231da

--- knowrob_common/src/edu/tum/cs/ias/knowrob/utils/ros/RosUtilities.java
+++ knowrob_common/src/edu/tum/cs/ias/knowrob/utils/ros/RosUtilities.java
@@ -1,10 +1,16 @@
 package edu.tum.cs.ias.knowrob.utils.ros;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.UUID;
+
+import com.google.common.base.Joiner;
 
 
 public class RosUtilities {
 
+	static HashMap<String, Process> processMap = new HashMap<String, Process>();
+	
     /**
      * Finds a ROS package using rospack and returns its path. 
      * @param name of the ROS package
@@ -31,6 +37,70 @@ public class RosUtilities {
             e.printStackTrace();
         }
         return path;
+    }
+    
+
+    /**
+     * Finds a ROS package using rospack and returns its path. 
+     * @param pkg name of the ROS package
+     * @param binary name of the binary
+     * @param args Arguments
+     * @return Handle to this process
+     * @throws IOException 
+     */
+    public static String rosrun(String pkg, String binary, String[] arg) throws IOException {
+
+        String handle = null;
+//        try
+//        {
+        	String args = Joiner.on(" ").join(arg);
+        	Process p = Runtime.getRuntime().exec("rosrun " + pkg + " " + binary + " " + args);
+        	
+//        	BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+        	
+//        	int lines = 10;
+//        	while(lines > 0) {
+//        		System.out.println(br.readLine());
+//        		lines --;
+//        	}
+        	
+//            Process p = new ProcessBuilder( "rosrun " + pkg + " " + binary + " " + pkg + " " ).start();
+            handle = UUID.randomUUID().toString();
+            processMap.put(handle, p);
+            
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+        return handle;
+    }
+    
+    /**
+     * Kill a process based on its UUID (which has been returned 
+     * by the RosUtilities.rosrun() method
+     * @param UUID of the process to be killed
+     * @param binary name of the binary
+     */
+    public static void kill(String handle) {
+
+//        try {
+    	System.out.println(processMap.get(handle).toString());
+            processMap.get(handle).destroy();
+            
+            
+//        } catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+    	
+    	RosUtilities.rosrun("map_server", "map_server", new String[]{"/work/tenorth/work/roboearth/ros_roboearth/re_ias_hospital_room/map/hospital_room.yaml"});
+    	
+    	System.out.print("bla");
+    	
     }
     
 }
