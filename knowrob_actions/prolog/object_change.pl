582d5caaa6d4ba1bd924daafe1aede6657383ae5
tenorth <tenorth@63041b90-49fb-4ca4-89d8-2ccc525231da>
2012-05-08 09:46:15 +0000

    fixing generation of owl from owl.in for knowrob_actions
    git-svn-id: http://code.in.tum.de/svn/knowrob/trunk@693 63041b90-49fb-4ca4-89d8-2ccc525231da

--- knowrob_actions/prolog/object_change.pl
+++ knowrob_actions/prolog/object_change.pl
@@ -17,7 +17,7 @@
 
 
 :- owl_parser:owl_parse('../owl/object-change.owl', false, false, true).
-:- owl_parser:owl_parse('../owl/pancake-making.owl', false, false, true).
+% :- owl_parser:owl_parse('../owl/pancake-making.owl', false, false, true).
 
 :- rdf_db:rdf_register_ns(knowrob,      'http://ias.cs.tum.edu/kb/knowrob.owl#',      [keep(true)]).
 :- rdf_db:rdf_register_ns(object_change, 'http://ias.cs.tum.edu/kb/object-change.owl#', [keep(true)]).
