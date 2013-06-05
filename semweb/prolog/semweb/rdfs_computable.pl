d06818739586872a16caafad5b001b6d62c7dc0b
tenorth <tenorth@63041b90-49fb-4ca4-89d8-2ccc525231da>
2013-06-05 14:45:26 +0000

    fixed bug in rdfs_computable.pl: check for type of an existing instance via comptutables was not supported in rdfs_instance_of
    git-svn-id: http://code.in.tum.de/svn/knowrob/trunk@1123 63041b90-49fb-4ca4-89d8-2ccc525231da

--- semweb/prolog/semweb/rdfs_computable.pl
+++ semweb/prolog/semweb/rdfs_computable.pl
@@ -230,13 +230,19 @@ rdfs_computable_instance_of(Instance, Class) :-
   ((findall(I, rdfs_computable_sql_instance_of(I,    Class), Is), member(Instance, Is)) ;
    (findall(I, rdfs_computable_prolog_instance_of(I, Class), Is), member(Instance, Is))).
 
-
 % determine classes
 rdfs_computable_instance_of(Instance, Class) :-
   var(Class),
   nonvar(Instance),
   ((findall(C, rdfs_computable_sql_instance_of(Instance,    C), Cs), member(Class, Cs));
    (findall(C, rdfs_computable_prolog_instance_of(Instance, C), Cs), member(Class, Cs))).
+
+% check type
+rdfs_computable_instance_of(Instance, Class) :-
+  nonvar(Class),
+  nonvar(Instance),
+  ((findall(C, rdfs_computable_sql_instance_of(Instance,    C), Cs), member(Cls, Cs), rdfs_subclass_of(Cls, Class));
+   (findall(C, rdfs_computable_prolog_instance_of(Instance, C), Cs), member(Cls, Cs), rdfs_subclass_of(Cls, Class))).
 
 
 
