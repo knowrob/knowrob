2941a21df7f8d21233063eba2b11b8ce50db6e14
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2013-07-09 13:40:23 +0200

    reworked semantic map utils: prolog-style predicate names, fixed queries to support multiple maps that are loaded at the same time

--- ias_semantic_map/prolog/semantic_map_utils.pl
+++ ias_semantic_map/prolog/semantic_map_utils.pl
@@ -1,5 +1,5 @@
 %%
-%% Copyright (C) 2009 by Lars Kunze, Lorenz Moesenlechner, Moritz Tenorth
+%% Copyright (C) 2009-13 by Lars Kunze, Lorenz Moesenlechner, Moritz Tenorth
 %%
 %% This program is free software; you can redistribute it and/or modify
 %% it under the terms of the GNU General Public License as published by
@@ -15,142 +15,180 @@
 %% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %%
 
-:- module(ias_semantic_map,
+:- module(semantic_map_utils,
     [
-      readMap/1,
-      readMap/2,
-      rdf_atom_no_ns/2,
-      withoutNamespace/2,
-      objectType/2,
-      rootObjects/1,
-      rootObject/1,
-      objectDimensions/4,
-      objectPose/2,
-      childObject/2,
-      childObjects/2,
-      objectLabel/2,
-      semanticMap/1,
-      connectionType/3,
-      jointName/3
+      map_instance/1,
+      map_read_all/1,
+      map_read_all/2,
+      map_object_info/1,
+      map_object_type/2,
+      map_root_objects/2,
+      map_root_object/2,
+      map_object_dimensions/4,
+      map_child_object/2,
+      map_child_objects/2,
+      map_object_label/2,
+      map_connection_type/3,
+      map_joint_name/3,
+      rdf_atom_no_ns/2
     ]).
 
-readMap([R0|EntityList]) :-
-    readMap([R0|EntityList], 'http://ias.cs.tum.edu/kb/knowrob.owl#SemanticEnvironmentMap0').
 
-readMap([R0|EntityList], MapInstance) :-
-    readEntity(MapInstance, 'null', R0),
-    setof(TopLevelObj,
-          ( owl_individual_of(TopLevelObj, knowrob:'StorageConstruct');
-            owl_individual_of(TopLevelObj, knowrob:'FurniturePiece');
-            owl_individual_of(TopLevelObj, knowrob:'CounterTop') ),
-          TopLevelObjs),
-    readEntities(TopLevelObjs, MapInstance, EntityList).
+:- use_module(library('semweb/rdfs')).
+:- use_module(library('thea/owl_parser')).
+:- use_module(library('semweb/owl')).
+:- use_module(library('semweb/rdfs_computable')).
+:- use_module(library('knowrob_objects')).
+:- use_module(library('knowrob_perception')).
 
 
-readEntities([], _, []).
-readEntities([E|Es], P, Res) :-
-  ((setof(Prt, rdf_has(E, knowrob:properPhysicalParts, Prt), Prts)) -> (
-    readEntity(E, P, R0),
-    readEntities(Prts, E, Rs),
+:-  rdf_meta
+      map_instance(r),
+      map_read_all(-),
+      map_read_all(-,r),
+      map_root_object(r,r),
+      map_root_objects(r,?),
+      map_child_object(r,r),
+      map_child_objects(r,?),
+      map_object_info(?),
+      map_object_type(r,r),
+      map_object_dimensions(r,?,?,?),
+      map_object_label(r,?),
+      map_connection_type(r,r,+),
+      map_joint_name(r,r,r),
+      rdf_atom_no_ns(r,?).
+
+
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Map root and child objects
+
+map_root_objects( Map, Objs ) :-
+    setof( Obj, map_root_object(Map, Obj), Objs).
+
+map_root_object( Map, Obj ) :-
+    owl_has(Obj, knowrob:describedInMap, Map),
+    (owl_individual_of(Obj, knowrob:'StorageConstruct');
+    owl_individual_of(Obj, knowrob:'WallOfAConstruction');
+    owl_individual_of(Obj, knowrob:'FurniturePiece');
+    owl_individual_of(Obj, knowrob:'CounterTop')).
+
+
+map_child_objects( Parent, Children ) :-
+    setof( Child, map_child_object(Parent, Child), Children).
+
+map_child_object( Parent, Child ) :-
+    owl_has( Parent, knowrob:'properPhysicalParts', Child ),
+    not( owl_individual_of(Child, knowrob:'Connection-Physical') ).
+
+    
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Read object properties
+
+
+map_object_info([Inst, Type, Pose, [D, W, H]]) :-
+    map_instance(Map),
+    map_root_object(Map, Inst),
+    map_object_type(Inst, Type),
+    current_object_pose(Inst, Pose),
+    map_object_dimensions(Inst, W, D, H).
+
+map_object_type(Obj, Type) :-
+    rdf_has( Obj, rdf:type, Type ),
+    owl_subclass_of( Type, knowrob:'SpatialThing').
+
+map_object_label( Obj, Label ) :-
+    owl_has( Obj, rdfs:label, literal(type('http://www.w3.org/2001/XMLSchema#string', Label)) ).
+
+map_object_dimensions( Obj, W, D, H ) :-
+    rdf_has( Obj, knowrob:'widthOfObject', literal(type(_, W_)) ),
+    atom_number(W_, W),
+    rdf_has( Obj, knowrob:'depthOfObject', literal(type(_, D_)) ),
+    atom_number(D_, D),
+    rdf_has( Obj, knowrob:'heightOfObject', literal(type(_, H_)) ),
+    atom_number(H_, H).
+
+map_object_dimensions( Obj, W, D, H ) :-
+    % The depth of a knob defaults to 3cm here. This information
+    % should either be asserted somewhere else or be set as a property
+    % when importing the semantic map.
+    rdf_has( Obj, knowrob:'radius', literal(type(_, Radius_)) ),
+    atom_number(Radius_, Radius),
+    W is 2 * Radius,
+    H is 2 * Radius,
+    D is 0.03.
+
+
+
+
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Read joints and connections between object parts
+
+
+% TODO: This does not seem correct, but I am not sure what these predicates
+%       are to do after all, so I let them like this for now (MT).
+map_connection_type( Parent, Child, 'HingedJoint' ) :-
+    rdf_has( Parent, knowrob:'properPhysicalParts', Child ),
+    rdf_has( Child, knowrob:'properPhysicalParts', Joint ),
+    owl_individual_of(Joint, knowrob:'HingedJoint'), !.
+
+map_connection_type( Parent, Parent, 'PrismaticJoint' ) :-
+    rdf_has( Parent, rdf:type, knowrob:'Drawer' ), !.
+
+map_connection_type( Parent, Child, 'Fixed' ) :-
+    rdf_has( Parent, knowrob:'properPhysicalParts', Child ).
+
+map_joint_name( Parent, Child, Name ) :-
+    rdf_has( Parent, knowrob:'properPhysicalParts', Child ),
+    rdf_has( Child, knowrob:'properPhysicalParts', Name ),
+    owl_individual_of( Name, knowrob:'Connection-Physical' ).
+
+
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Read map instance and content
+
+map_instance( Map ) :-
+    rdf_has(Map, rdf:type, knowrob:'SemanticEnvironmentMap').
+
+map_read_all([R0|EntityList]) :-
+    map_read_all([R0|EntityList], 'http://ias.cs.tum.edu/kb/ias_semantic_map.owl#SemanticEnvironmentMap0').
+
+map_read_all([R0|EntityList], MapInstance) :-
+    map_read_entity(MapInstance, 'null', R0),
+    setof(TopLevelObj, owl_has(TopLevelObj, knowrob:describedInMap, MapInstance), TopLevelObjs),
+    map_read_entities(TopLevelObjs, MapInstance, EntityList).
+
+map_read_entities([], _, []).
+map_read_entities([E|Es], P, Res) :-
+  ((setof(Part, rdf_has(E, knowrob:properPhysicalParts, Part), Parts)) -> (
+    map_read_entity(E, P, R0),
+    map_read_entities(Parts, E, Rs),
     append([R0], Rs, R)
   );(
-    readEntity(E, P, R1),
+    map_read_entity(E, P, R1),
     R=[R1]
   )),
-  readEntities(Es, P, Res1),
+  map_read_entities(Es, P, Res1),
   append(R, Res1, Res).
 
 
-readEntity(E, Parent, [E_no_ns, Parent_no_ns, Pose]) :-
+map_read_entity(E, Parent, [E_no_ns, Parent_no_ns, Pose]) :-
     rdf_atom_no_ns(E, E_no_ns),
     ((Parent = 'null') ->
      (Parent_no_ns = Parent) ;
      rdf_atom_no_ns(Parent, Parent_no_ns)),
-    objectPose( E, Pose ).
+    current_object_pose( E, Pose ).
 
-rdf_atom_no_ns( Tin, Tout ) :-
-    rdf_split_url(_, Tout, Tin ).
 
-withoutNamespace( Tin, Tout ) :-
-    rdf_split_url(_, Tout, Tin).
 
-objectType(O, T) :-
-    rdf_has( O, rdf:type, T ),
-    owl_subclass_of( T, knowrob:'SpatialThing').
 
-semanticMap( Map ) :-
-    rdf_has(Map, rdf:type, knowrob:'SemanticEnvironmentMap').
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Utility methods
 
-rootObjects( Os ) :-
-    setof( O, rootObject(O), Os).
+rdf_atom_no_ns( IRI, LocalName ) :-
+    rdf_split_url(_, LocalName, IRI ).
 
-rootObject( O ) :-
-    owl_individual_of(O, knowrob:'StorageConstruct');
-    owl_individual_of(O, knowrob:'WallOfAConstruction');
-    owl_individual_of(O, knowrob:'FurniturePiece');
-    owl_individual_of(O, knowrob:'CounterTop').
 
-objectInfo([Uri, Type, Pose, [D, W, H]]) :-
-    rootObject(Uri),
-    objectType(Uri, Type),
-    objectPose(Uri, Pose),
-    objectDimensions(Uri, W, D, H).
-
-objectDimensions( O, W, D, H ) :-
-    rdf_has( O, knowrob:'widthOfObject', literal(type(_, W_)) ),
-    atom_number(W_, W),
-    rdf_has( O, knowrob:'depthOfObject', literal(type(_, D_)) ),
-    atom_number(D_, D),
-    rdf_has( O, knowrob:'heightOfObject', literal(type(_, H_)) ),
-    atom_number(H_, H).
-
-objectDimensions( O, W, D, H ) :-
-    % The depth of a knob defaults to 3cm here. This information
-    % should either be asserted somewhere else or be set as a property
-    % when importing the semantic map.
-    rdf_has( O, knowrob:'radius', literal(type(_, R_)) ),
-    atom_number(R_, R),
-    W is 2 * R,
-    H is 2 * R,
-    D is 0.03.
-
-childObject( P, C ) :-
-    rdf_has( P, knowrob:'properPhysicalParts', C ),
-    not( owl_individual_of(C, knowrob:'Connection-Physical') ).
-
-objectLabel( O, L ) :-
-    owl_has( O, rdfs:label, literal(type('http://www.w3.org/2001/XMLSchema#string', L)) ).
-
-connectionType( P, C, 'HingedJoint' ) :-
-    rdf_has( P, knowrob:'properPhysicalParts', C ),
-    rdf_has( C, knowrob:'properPhysicalParts', J ),
-    owl_individual_of(J, knowrob:'HingedJoint'), !.
-
-connectionType( P, P, 'SliderJoint' ) :-
-    rdf_has( P, rdf:type, knowrob:'Drawer' ), !.
-
-connectionType( P, C, 'Fixed' ) :-
-    rdf_has( P, knowrob:'properPhysicalParts', C ).
-
-jointName( P, C, Name ) :-
-    rdf_has( P, knowrob:'properPhysicalParts', C ),
-    rdf_has( C, knowrob:'properPhysicalParts', Name ),
-    owl_individual_of( Name, knowrob:'Connection-Physical' ).
-
-childObjects( P, Cs ) :-
-    setof( C, childObject(P, C), Cs).
-
-objectPose( O, P ) :-
-    rdf_triple(knowrob:orientation, O, Pobj ),
-    bagof( V,
-           X^Y^(member(Y, [0, 1, 2, 3]),
-                member(X, [0, 1, 2, 3]),
-                matrixValue( Pobj, X, Y, V )),
-           P ).
-
-matrixValue( M, X, Y, V ) :-
-    concat( m, Y, Tmp ),
-    concat( Tmp, X, Name ),
-    rdf_global_id(knowrob:Name, FullName),
-    rdf_triple( FullName, M, literal(type(_, V_)) ),
-    atom_number( V_, V ).
