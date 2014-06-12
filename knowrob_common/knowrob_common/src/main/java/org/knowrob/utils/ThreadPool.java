9f4cdee3f1b4050647f8942d6e99c705b3636f17
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2014-06-12 12:56:39 +0200

    rosjava config for knowrob_common

---
+++ knowrob_common/knowrob_common/src/main/java/org/knowrob/utils/ThreadPool.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Stefan Profanter. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the GNU Public License v3.0 which accompanies
+ * this distribution, and is available at http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors: Stefan Profanter - initial API and implementation, Year: 2012
+ ******************************************************************************/
+package org.knowrob.utils;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Simple Thread execution pool to execute multiple callable objects at same time.
+ * 
+ * @author Stefan Profanter
+ * 
+ */
+public class ThreadPool {
+
+	/**
+	 * Executes the given callable objects in a thread pool and returns when all threads have
+	 * finished and all callable objects have been executed.
+	 * 
+	 * @param threads
+	 *            list of callable objects
+	 */
+
+	public static void executeInPool(List<Callable<Void>> threads) {
+		executeInPool(threads, -1);
+	}
+	public static void executeInPool(List<Callable<Void>> threads, int numParallel) {
+		ExecutorService	pool;
+		int threadNum = numParallel <= 0 ? Runtime.getRuntime().availableProcessors() * 2 : numParallel;
+		pool = Executors.newFixedThreadPool(threadNum);
+
+		try {
+			List<Future<Void>> futures = pool.invokeAll(threads);
+			for (Future<Void> f : futures) {
+				try {
+					f.get(); // If called thread threw an exception, get will throw an
+								// ExecutionException
+				} catch (ExecutionException ex) {
+					ex.getCause().printStackTrace(); // Print exceptions from called threads if
+														// there were any
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		threads.clear();
+	}
+}
---
+++ knowrob_common/knowrob_common/src/main/java/org/knowrob/utils/ros/RosUtilities.java
@@ -0,0 +1,92 @@
+package org.knowrob.utils.ros;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.UUID;
+
+import com.google.common.base.Joiner;
+
+
+public class RosUtilities {
+
+	static HashMap<String, Process> processMap = new HashMap<String, Process>();
+	
+    /**
+     * Finds a ROS package using rospack and returns its path. 
+     * @param name of the ROS package
+     * @return path to the package - if it was found<br>
+     * <tt>null</tt> - otherwise
+     */
+    public static String rospackFind(String pkg) {
+
+        String path = null;
+        try {
+        	
+       	
+            Process p = Runtime.getRuntime().exec("rospack find " + pkg);
+            
+            if(p.waitFor()==127) {
+            //	throw new RuntimeException("External program 'rospack' not found");
+            }
+
+            BufferedReader out = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader err = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            if ((path = out.readLine()) != null){
+                ;//System.out.println("Package: " + pkg + ", Path: " + path);                    
+            } else {
+            	
+            	// print error output
+            	String l = null;
+            	while ( (l = err.readLine()) != null)
+                    System.out.println(l);
+            	
+                ;//System.out.println("Package: " + pkg + ", Error: package not found!");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return path;
+    }
+    
+
+    /**
+     * Finds a ROS package using rospack and returns its path. 
+     * @param pkg name of the ROS package
+     * @param binary name of the binary
+     * @param args Arguments
+     * @return Handle to this process
+     * @throws IOException 
+     */
+    public static String rosrun(String pkg, String binary, String[] arg) throws IOException {
+
+        String handle = null;
+        try
+        {
+        	String args = Joiner.on(" ").join(arg);
+        	Process p = Runtime.getRuntime().exec("rosrun " + pkg + " " + binary + " " + args);
+
+        	handle = UUID.randomUUID().toString();
+            processMap.put(handle, p);
+            
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return handle;
+    }
+    
+    /**
+     * Kill a process based on its UUID (which has been returned 
+     * by the RosUtilities.rosrun() method
+     * @param UUID of the process to be killed
+     * @param binary name of the binary
+     */
+    public static void kill(String handle) {
+    	processMap.get(handle).destroy();
+    }
+    
+}
