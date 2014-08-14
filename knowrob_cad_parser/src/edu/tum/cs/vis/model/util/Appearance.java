f80d58d5336f1329fd1c8c214fa6d90c6e6a4e00
Andrei Stoica <stoica.razvan.andrei@gmail.com>
2014-08-14 09:40:33 +0200

    Added empty constructor and copy constructor: * implemented copy constructor to perform copies of Appearance objects for newly added triangles in the knowrob_mesh_reasoning package

--- knowrob_cad_parser/src/edu/tum/cs/vis/model/util/Appearance.java
+++ knowrob_cad_parser/src/edu/tum/cs/vis/model/util/Appearance.java
@@ -1,12 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2012 Stefan Profanter.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
+ * Copyright (c) 2012 Stefan Profanter. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the GNU Public License v3.0 which accompanies
+ * this distribution, and is available at http://www.gnu.org/licenses/gpl.html
  * 
- * Contributors:
- *     Stefan Profanter - initial API and implementation, Year: 2012
+ * Contributors: Stefan Profanter - initial API and implementation, Year: 2012
  ******************************************************************************/
 package edu.tum.cs.vis.model.util;
 
@@ -30,27 +27,59 @@ public class Appearance implements Serializable {
 	/**
 	 * the Filename of the texture-Image
 	 */
-	private String				imageFileName		= null;
+	private String				imageFileName;
 
 	/**
 	 * an Object reference of a loaded image. Default is null.
 	 */
-	private PImage				imageReference		= null;
+	private PImage				imageReference;
 
 	/**
 	 * the color of the Triangle if the Triangle has no texture Set to null if no fill needed
 	 */
-	private Color				colorFill			= null;
+	private Color				colorFill;
 
 	/**
 	 * the color stroke for Triangle or Line Set to null if no stroke needed
 	 */
-	private Color				colorLine			= null;
+	private Color				colorLine;
 
 	/**
 	 * Thickness of the stroke
 	 */
-	private int					strokeWeight		= 2;
+	private int					strokeWeight;
+
+	/**
+	 * Empty constructor for appearance objects
+	 */
+	public Appearance() {
+		imageFileName = null;
+		imageReference = null;
+		colorFill = null;
+		colorLine = null;
+		strokeWeight = 2;
+	};
+
+	/**
+	 * Copy constructor for the Appearance objects
+	 * 
+	 * @param toCopy
+	 *            object to copy to newly created instance
+	 */
+	public Appearance(final Appearance toCopy) {
+		if (toCopy.colorFill != null) {
+			this.colorFill = toCopy.colorFill;
+		}
+		if (toCopy.colorLine != null) {
+			this.colorLine = toCopy.colorLine;
+		}
+		if (toCopy.imageFileName != null) {
+			this.imageFileName = toCopy.imageFileName;
+		}
+		if (toCopy.imageReference != null) {
+			this.imageReference = toCopy.imageReference;
+		}
+	}
 
 	/**
 	 * @return the colorFill
@@ -126,5 +155,4 @@ public class Appearance implements Serializable {
 	public void setStrokeWeight(int strokeWeight) {
 		this.strokeWeight = strokeWeight;
 	}
-
 }
