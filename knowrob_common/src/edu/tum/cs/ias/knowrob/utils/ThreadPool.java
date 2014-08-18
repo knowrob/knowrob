2e3e320cd89199abc6d7283b0354265c25865ccd
Andrei Stoica <stoica.razvan.andrei@gmail.com>
2014-08-18 19:09:30 +0200

    Brushed up implementation of common utils: * documentation improvement to acknowledge minor refactors for history completeness and possible future reference

--- knowrob_common/src/edu/tum/cs/ias/knowrob/utils/ThreadPool.java
+++ knowrob_common/src/edu/tum/cs/ias/knowrob/utils/ThreadPool.java
@@ -3,7 +3,9 @@
  * materials are made available under the terms of the GNU Public License v3.0 which accompanies
  * this distribution, and is available at http://www.gnu.org/licenses/gpl.html
  * 
- * Contributors: Stefan Profanter - initial API and implementation, Year: 2012
+ * Contributors: 
+ * 				Stefan Profanter - initial API and implementation, Year: 2012
+ * 				Andrei Stoica - minor refactor during Google Summer of Code 2014
  ******************************************************************************/
 package edu.tum.cs.ias.knowrob.utils;
 
@@ -18,7 +20,7 @@ import java.util.concurrent.Future;
  * Simple Thread execution pool to execute multiple callable objects at same time.
  * 
  * @author Stefan Profanter
- * 
+ * @author Andrei Stoica - shutdown pool after it ended to avoid leaks
  */
 public class ThreadPool {
 
