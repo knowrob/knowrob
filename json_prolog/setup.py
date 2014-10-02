41aa6fe810434442dae98c83c851f8e1b572f135
Daniel Beßler <danielb@informatik.uni-bremen.de>
2014-10-02 12:09:03 +0200

    Fix for install procedure from Benjamin

--- json_prolog/setup.py
+++ json_prolog/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['json_prolog'],
-    package_dir={'': 'src/json_prolog'},
+    package_dir={'': 'src'},
 )
 
 setup(**setup_args)
