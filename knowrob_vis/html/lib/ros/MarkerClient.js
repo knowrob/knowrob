35c61f5bf15f301784ed4d63816bb24990a53322
Daniel Beßler <danielb@informatik.uni-bremen.de>
2014-10-13 11:50:39 +0200

    Consider the namespace of markers in order to avoid unwanted collisions between marker id's.

--- knowrob_vis/html/lib/ros/MarkerClient.js
+++ knowrob_vis/html/lib/ros/MarkerClient.js
@@ -40,14 +40,14 @@ ROS3D.MarkerClient = function(options) {
     });
 
     // remove old marker from Three.Object3D children buffer
-    that.rootObject.remove(that.markers[message.id]);
+    that.rootObject.remove(that.markers[message.ns + message.id]);
 
-    that.markers[message.id] = new ROS3D.SceneNode({
+    that.markers[message.ns + message.id] = new ROS3D.SceneNode({
       frameID : message.header.frame_id,
       tfClient : that.tfClient,
       object : newMarker
     });
-    that.rootObject.add(that.markers[message.id]);
+    that.rootObject.add(that.markers[message.ns + message.id]);
 
     that.emit('change');
   });
@@ -58,14 +58,14 @@ ROS3D.MarkerClient = function(options) {
       message : message
     });
 
-    that.rootObject.remove(that.markers[message.id]);
+    that.rootObject.remove(that.markers[message.ns + message.id]);
 
-    that.markers[message.id] = new ROS3D.SceneNode({
+    that.markers[message.ns + message.id] = new ROS3D.SceneNode({
       frameID : message.header.frame_id,
       tfClient : that.tfClient,
       object : newMarker
     });
-    that.rootObject.add(that.markers[message.id]);
+    that.rootObject.add(that.markers[message.ns + message.id]);
 
     that.emit('change');
   };
