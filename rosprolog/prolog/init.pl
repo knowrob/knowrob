aad7ed6088eadd7be362724fc20300a51fa7475e
Moritz Tenorth <knowrob@tenorth.de>
2014-12-12 16:46:50 +0100

    added documentation for the rosprolog init scripts

--- rosprolog/prolog/init.pl
+++ rosprolog/prolog/init.pl
@@ -1,6 +1,13 @@
 
 :- use_module(library('process')).
 
+%% rospack_package_path(+Package, -Path) is nondet.
+%
+% Locate ROS packages on the harddisk using 'rospack find'
+% 
+% @param Package  Name of a ROS package
+% @param Path     Global path of Package determined by 'rospack find'
+% 
 rospack_package_path(Package, Path) :-
   nonvar(Package),
   process_create(path('rospack'), ['find', Package], [stdout(pipe(RospackOutput)), process(PID)]),
@@ -8,6 +15,12 @@ rospack_package_path(Package, Path) :-
   string_to_list(Path, C),
   process_wait(PID, _).
 
+%% init_ros_package(+PackagePath) is nondet.
+%
+% Initialize a KnowRob package by consulting the prolog/init.pl file
+% 
+% @param PackagePath  Path towards the package to be initialized
+% 
 init_ros_package( PackagePath ) :-
   atom_concat(PackagePath, 'init.pl', InitFile),
   exists_file(InitFile),
@@ -15,11 +28,21 @@ init_ros_package( PackagePath ) :-
 
 init_ros_package( _ ).
 
-register_ros_package( Package, _ ) :-
+%% register_ros_package(+Package) is nondet.
+%% register_ros_package(+Package, ?AbsoluteDirectory) is nondet.
+%
+% Find and initialize a KnowRob package, i.e. locate the package on the
+% harddisk, add the path to the library search path, and consult the init.pl
+% file that (recursively) initializes the package and its dependencies.
+% 
+% @param Package            Name of a ROS package
+% @param AbsoluteDirectory  Global path to the 'prolog' subdirectory in the given package
+% 
+register_ros_package(Package, _) :-
   current_predicate(ros_package_initialized/1),
   ros_package_initialized(Package), !.
 
-register_ros_package( Package, AbsoluteDirectory ) :-
+register_ros_package(Package, AbsoluteDirectory) :-
   rospack_package_path(Package, PackagePath),
   nonvar(PackagePath),
   atom_concat(PackagePath, '/prolog/', AbsoluteDirectory),
@@ -30,21 +53,34 @@ register_ros_package( Package, AbsoluteDirectory ) :-
   init_ros_package( AbsoluteDirectory ).
 
 
-register_ros_package( Package ) :-
-  register_ros_package( Package, _ ).
+register_ros_package(Package) :-
+  register_ros_package(Package, _).
 
-use_ros_module( Package, FilePath ) :-
+use_ros_module(Package, FilePath) :-
   register_ros_package(Package, AbsoluteDirectory),
   atom_concat(AbsoluteDirectory, FilePath, AbsoluteFilePath),
   use_module( AbsoluteFilePath ).
 
-% adds java dependencies to classpath
+
+
+%% add_ros_package_to_classpath(+Package) is nondet.
+% 
+% Adds Java dependencies of Package to the CLASSPATH environment variable
+%
+% @param Package Name of a ROS package
+% 
 add_ros_package_to_classpath(Package):-
 	rospack_package_classpath(Package, Path),
 	atom_concat(':',Path,PackagePath),
 	setenv("CLASSPATH",PackagePath).
 
-% calculates java dependencies for classpath
+%% rospack_package_classpath(+Package, -Path) is nondet.
+% 
+% Calculates the Java dependencies of Package and returns a string to be appended to the CLASSPATH
+%
+% @param Package  Name of a ROS package
+% @param Path     String with the dependencies to be added to the CLASSPATH
+% 
 rospack_package_classpath(Package, Path) :-
   nonvar(Package),
   process_create(path('rosrun'), ['rosprolog', 'get_pkg_classpath', Package], [stdout(pipe(RospackOutput)), process(PID)]),
