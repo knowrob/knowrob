e52ebc1930596b559b3853cebfda007f3464e240
Daniel Beßler <danielb@tzi.de>
2015-02-22 23:48:24 +0100

    Minor fix

--- knowrob_mongo/knowrob_mongo/src/main/java/org/knowrob/interfaces/mongo/types/Designator.java
+++ knowrob_mongo/knowrob_mongo/src/main/java/org/knowrob/interfaces/mongo/types/Designator.java
@@ -185,8 +185,7 @@ public class Designator {
 		String[] parts = s.split("_");
 		String camelCaseString = "";
 		for (String part : parts){
-			camelCaseString += camelCaseString +
-				part.substring(0, 1).toUpperCase() + part.substring(1);
+			camelCaseString += part.substring(0, 1).toUpperCase() + part.substring(1);
 		}
 		return camelCaseString;
 	}
