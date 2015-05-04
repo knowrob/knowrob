75df6827c585bda857fa4ce690482c39ede4ec7f
Daniel Beßler <danielb@informatik.uni-bremen.de>
2015-05-04 20:06:26 +0200

    Hacking on math utils

--- knowrob_common/knowrob_common/src/main/java/org/knowrob/utils/MathUtil.java
+++ knowrob_common/knowrob_common/src/main/java/org/knowrob/utils/MathUtil.java
@@ -2,8 +2,10 @@ package org.knowrob.utils;
 
 import java.util.LinkedList;
 
+import javax.vecmath.Matrix3d;
 import javax.vecmath.Matrix4d;
 import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
 
 public class MathUtil {
 	/**
@@ -33,6 +35,77 @@ public class MathUtil {
 		
 		return new double[] { quat.w, quat.x, quat.y, quat.z };
 	}
+
+	public static double[] orientationToQuaternion(float[] dir) {
+		return orientationToQuaternion(new Vector3d(dir[0], dir[1], dir[2]));
+	}
+
+	public static double[] orientationToQuaternion(double[] dir) {
+		return orientationToQuaternion(new Vector3d(dir[0], dir[1], dir[2]));
+	}
+
+	public static double[] orientationToQuaternion(Vector3d dir) {
+		/*
+		Vector3d up = new Vector3d(0.0, -1.0, 0.0);
+
+		Vector3d bz = new Vector3d();
+		bz.normalize(dir);
+
+		Vector3d bx = new Vector3d();
+		bx.cross(up, bz);
+		bx.normalize();
+
+		Vector3d by = new Vector3d();
+		by.cross(bx, bz);
+		by.normalize();
+
+		Matrix3d mat = new Matrix3d(
+			bx.x, bx.y, bx.z,
+			by.x, by.y, by.z,
+			bz.x, bz.y, bz.z
+		);
+		*/
+
+		Vector3d up = new Vector3d(0.0, 1.0, 0.0);
+
+		Vector3d bz = new Vector3d();
+		bz.normalize(dir);
+
+		Vector3d bx = new Vector3d();
+		bx.cross(up, bz);
+		bx.normalize();
+
+		Vector3d by = new Vector3d();
+		by.cross(bx, bz);
+		by.normalize();
+
+		Matrix3d mat = new Matrix3d(
+			bz.x, bz.y, bz.z,
+			bx.x, bx.y, bx.z,
+			by.x, by.y, by.z
+		);
+		
+		Matrix3d rotXMat = new Matrix3d();
+		rotXMat.setIdentity();
+		rotXMat.rotY(Math.PI);
+		//rotXMat.mul(mat);
+		mat.mul(rotXMat);
+		
+		/*
+		Matrix3d mat = new Matrix3d(
+			bx.x, by.x, bz.x, 
+			bx.y, by.y, bz.y, 
+			bx.z, by.z, bz.z
+		);
+		*/
+		
+		Quat4d q = new Quat4d();
+		q.set(mat);
+		
+		return new double[] {
+			q.w, q.x, q.y, q.z
+		};
+	}
 	
 	/**
 	 * Read double array from input string.
@@ -61,5 +134,11 @@ public class MathUtil {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	public static void main(String[] args) {
+		Vector3d dir = new Vector3d(1.0, 2.0, 3.0);
+		double[] q = orientationToQuaternion(dir);
+		System.out.println("q: " + q[0] + " " + q[1] + " " + q[2] + " " + q[3]);
 	}
 }
