knowrob/knowrob

commit 3b6d7681a26f1583ceb863fa7c94c20eac0072bb

Extended diagram publisher to allow labels per data row (used for visualization of multiple timelines in one chart)

knowrob_vis/knowrob_vis/src/main/java/org/knowrob/vis/DiagramVisualization.java
@@ -86,6 +86,10 @@ public class DiagramVisualization extends AbstractNodeMain {
 	 *
 	 */
 	public void addDiagram(String id, String title, String type, String xlabel, String ylabel, int width, int height, String fontsize, String[][][] values) {
+		this.addDiagram(id, title, type, xlabel, ylabel, width, height, fontsize, null, values);
+	}
+
+	public void addDiagram(String id, String title, String type, String xlabel, String ylabel, int width, int height, String fontsize, String[] labels, String[][][] values) {
 
 		DataVis data = node.getTopicMessageFactory().newFromType(data_vis_msgs.DataVis._TYPE);
 
@@ -103,14 +107,26 @@ public class DiagramVisualization extends AbstractNodeMain {
 			data.setType(DataVis.TYPE_BARCHART);
 		} else if(type.equals("treechart")) {
 			data.setType(DataVis.TYPE_TREECHART);
-                } else if(type.equals("timeline")) {
-                        data.setType(DataVis.TYPE_TIMELINE);
-                } else if(type.equals("linechart")) {
-                        data.setType(DataVis.TYPE_LINECHART);
+		} else if(type.equals("timeline")) {
+			data.setType(DataVis.TYPE_TIMELINE);
+		} else if(type.equals("linechart")) {
+			data.setType(DataVis.TYPE_LINECHART);
 		}
 
-		for(String[][] val_list : values) {
+		if(labels.length != values.length) {
+			System.err.println("ERROR: Number of labels and data rows differs.");
+		}
+
+		for(int i =0; i< values.length; i++) {
+			
+			String[][] val_list = values[i];
+
 			ValueList v = node.getTopicMessageFactory().newFromType(data_vis_msgs.ValueList._TYPE);
+
+			if(labels != null) {
+				v.setLabel(labels[i]);
+			}
+
 			v.setValue1(new ArrayList<String>(Arrays.asList(val_list[0])));
 			v.setValue2(new ArrayList<String>(Arrays.asList(val_list[1])));
 			data.getValues().add(v);

knowrob_vis/prolog/knowrob_vis.pl
@@ -93,6 +93,7 @@
       diagram_canvas/0,
       clear_diagram_canvas/0,
       add_diagram/9,
+      add_diagram/10,
       add_barchart/3,
       add_piechart/3,
       remove_diagram/1,
@@ -842,13 +843,18 @@ diagram_canvas(Canvas) :-
 % @param Width      Width of the diagram in px
 % @param Height     Height of the diagram in px
 % @param Fontsize   Fontsize for the labels
+% @param RowLabels  List of labels for the data rows (optional)
 % @param ValueList  List of data ranges, each of the form [[a,b],['1','2']]
 %
 add_diagram(Id, Title, Type, Xlabel, Ylabel, Width, Height, Fontsize, ValueList) :-
+  add_diagram(Id, Title, Type, Xlabel, Ylabel, Width, Height, Fontsize, @(null), ValueList).
+  
+add_diagram(Id, Title, Type, Xlabel, Ylabel, Width, Height, Fontsize, RowLabels, ValueList) :-
   once((
     d_canvas(Canvas),
     lists_to_arrays(ValueList, ValueArr),
-    jpl_call(Canvas, 'addDiagram', [Id, Title, Type, Xlabel, Ylabel, Width, Height, Fontsize, ValueArr], _)
+    lists_to_arrays(RowLabels, RowLabelArr),
+    jpl_call(Canvas, 'addDiagram', [Id, Title, Type, Xlabel, Ylabel, Width, Height, Fontsize, RowLabelArr, ValueArr], _)
   )).
 
 %% add_piechart(+Id, +Title, +ValueList) is nondet.

commit 22ed8fbc2188beb3da7fcf54d769e572050a296f

Fixed diagram label bug introduced in 3b6d7681a26f1583ceb863fa7c94c20eac0072bb

knowrob_vis/knowrob_vis/src/main/java/org/knowrob/vis/DiagramVisualization.java
@@ -113,7 +113,7 @@ public class DiagramVisualization extends AbstractNodeMain {
 			data.setType(DataVis.TYPE_LINECHART);
 		}
 
-		if(labels.length != values.length) {
+		if(labels!=null && labels.length != values.length) {
 			System.err.println("ERROR: Number of labels and data rows differs.");
 		}
 

knowrob_vis/prolog/knowrob_vis.pl
@@ -854,7 +854,7 @@ add_diagram(Id, Title, Type, Xlabel, Ylabel, Width, Height, Fontsize, RowLabels,
   once((
     d_canvas(Canvas),
     lists_to_arrays(ValueList, ValueArr),
-    lists_to_arrays(RowLabels, RowLabelArr),
+    once((lists_to_arrays(RowLabels, RowLabelArr) ; RowLabelArr = @(null))),
     jpl_call(Canvas, 'addDiagram', [Id, Title, Type, Xlabel, Ylabel, Width, Height, Fontsize, RowLabelArr, ValueArr], _)
   )).
 