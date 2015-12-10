ad2d2d8026c978e69d22cd0f5314da0b49a6c5d9
Daniel Beßler <danielb@cs.uni-bremen.de>
2015-12-10 20:41:49 +0100

    minor fix; changed query group names

--- knowrob_vis/prolog/knowrob_marker.pl
+++ knowrob_vis/prolog/knowrob_marker.pl
@@ -71,6 +71,7 @@
 :- use_module(library('semweb/rdf_db')).
 :- use_module(library('rdfs_computable')).
 :- use_module(library('jpl')).
+:- use_module(library('knowrob_math')).
 :- use_module(library('lists')). % for sum_list
 
 :- rdf_meta marker(t,?),
