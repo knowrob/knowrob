1938c7cb0ce0e5fa27da9f6b1015644822a7fdfb
Asil Kaan Bozcuoglu <asil@cs.uni-bremen.de>
2017-09-21 17:07:06 +0200

    mongolookups for markers from tf according to new quartenion order. In the future, we should update the quartenions in OWLs according to new order as well for making a change in more generic level(in MarkerObject)

--- knowrob_objects/prolog/knowrob_objects.pl
+++ knowrob_objects/prolog/knowrob_objects.pl
@@ -300,7 +300,7 @@ object_pose_holds(Obj, Time, Pose, Interval) :-
 % TransformationMatrix
 object_pose(Matrix, _, pose([X,Y,Z], [QW,QX,QY,QZ])) :-
   is_list(Matrix), !,
-  matrix_rotation(Matrix, [QW,QX,QY,QZ]),
+  matrix_rotation(Matrix, [QX,QY,QZ,QW]),
   matrix_translation(Matrix, [X,Y,Z]).
 
 object_pose(Matrix, _, mat(Matrix)) :-
@@ -311,7 +311,7 @@ object_pose(Matrix, _, mat(Matrix)) :-
 object_pose(Pose, Instant, pose([X,Y,Z], [QW,QX,QY,QZ])) :-
   rdf_has(Pose, srdl2comp:'urdfName', literal(URDFName)),
   mng_lookup_transform('/map', URDFName, Instant, Transform), % FIXME: /map
-  matrix_rotation(Transform, [QW,QX,QY,QZ]),
+  matrix_rotation(Transform, [QX,QY,QZ,QW]),
   matrix_translation(Transform, [MX,MY,MZ]),
   % apply offset if specified
   ( position_to_list(Pose, [OX,OY,OZ])
@@ -340,7 +340,7 @@ object_pose(Pose, _, mat(Mat)) :-
 object_pose(Pose, _, pose([X,Y,Z], [QW,QX,QY,QZ])) :-
   rdfs_individual_of(Pose, knowrob:'Matrix'),
   rotmat_to_list(Pose, Matrix),
-  matrix_rotation(Matrix, [QW,QX,QY,QZ]),
+  matrix_rotation(Matrix, [QX,QY,QZ,QW]),
   matrix_translation(Matrix, [X,Y,Z]), !.
 
 object_pose(Pose, _, mat(Matrix)) :-
