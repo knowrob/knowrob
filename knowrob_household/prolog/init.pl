ec692fafa11627e9b31103653789f8e857e17da5
Daniel Beßler <danielb@cs.uni-bremen.de>
2017-12-18 11:12:46 +0100

    moved utility predicates from knowrob_maps to knowrob_household

--- knowrob_household/prolog/init.pl
+++ knowrob_household/prolog/init.pl
@@ -2,4 +2,6 @@
 :- register_ros_package(knowrob_common).
 :- register_ros_package(knowrob_actions).
 
+:- use_module(library('household')).
+
 :- owl_parser:owl_parse('package://knowrob_household/owl/household.owl').
