800889475980f83b3c86c917693f2a07d833682d
Daniel Beßler <danielb@cs.uni-bremen.de>
2017-12-27 14:45:51 +0100

    use rdfs_computable_triple_during

--- knowrob_common/prolog/knowrob/units.pl
+++ knowrob_common/prolog/knowrob/units.pl
@@ -39,81 +39,64 @@
 :- use_module(library('semweb/owl_parser')).
 :- use_module(library('semweb/owl')).
 :- use_module(library('knowrob/computable')).
+:- use_module(library('knowrob/temporal')).
 
 :- rdf_meta(convert_to_unit(r,r,r)).
 
 :- owl_parse('package://knowrob_common/owl/knowrob_units.owl').
 
-:- rdf_db:rdf_register_ns(rdf, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', [keep(true)]).
-:- rdf_db:rdf_register_ns(owl, 'http://www.w3.org/2002/07/owl#', [keep(true)]).
 :- rdf_db:rdf_register_ns(knowrob, 'http://knowrob.org/kb/knowrob.owl#', [keep(true)]).
-
 :- rdf_db:rdf_register_ns(knowrob_units, 'http://knowrob.org/kb/knowrob_units.owl#', [keep(true)]).
-:- rdf_db:rdf_register_ns(xsd, 'http://www.w3.org/2001/XMLSchema#', [keep(true)]).
 :- rdf_db:rdf_register_ns(qudt, 'http://qudt.org/schema/qudt#', [keep(true)]).
 :- rdf_db:rdf_register_ns(unit, 'http://qudt.org/vocab/unit#', [keep(true)]).
 :- rdf_db:rdf_register_ns(quantity, 'http://qudt.org/vocab/quantity#', [keep(true)]).
 :- rdf_db:rdf_register_ns(qudt-dimension, 'http://qudt.org/vocab/dimension#', [keep(true)]).
 :- rdf_db:rdf_register_ns(qudt-dimensionalunit, 'http://qudt.org/1.1/vocab/dimensionalunit#', [keep(true)]).
 
-
-% The rdf_triple hook provides transparent integration of the conversion routine
+% This hook provides transparent integration of the conversion routine
 % into the query process. If the user queries for results in a given unit, and
 % there are results in a different, but compatible unit (e.g. meters and
 % centimeters), they are converted and returned.
+rdfs_computable:rdfs_computable_triple_during(Property, Frame, literal(type(OutputType, OutputVal)), Interval) :-
+  % if value is partially given (e.g. literal(type(unit:Meter, Val)) ),
+  % try to call the respective computable without the binding and to
+  % convert the results into the correct form
+  ground([Property,Frame,OutputType]),
+  rdf_triple_during(Property, Frame, TempVal, Interval),
+  % try to convert the results into the specified format
+  convert_to_unit_internal(TempVal, OutputType, OutputVal).
 
-:- user:expand_term((rdf_triple_hook(Property, Frame, literal(type(OutputType, OutputVal))):-
-        % if value is partially given (e.g. literal(type(unit:Meter, Val)) ),
-        % try to call the respective computable without the binding and to
-        % convert the results into the correct form
-        ground([Property,Frame,OutputType]),
-        rdf_triple(Property, Frame, TempVal),
-        % try to convert the results into the specified format
-        convert_to_unit(TempVal, OutputType, OutputVal)), X),
-    assert(user:(X)).
+convert_to_unit_internal(literal(type(Type, _)), Type, _) :- fail, !.
+convert_to_unit_internal(literal(type(InputType, InputVal)), OutputType, Res) :-
+  convert_to_unit(literal(type(InputType, InputVal)), OutputType, Res).
 
-
+%% convert_to_unit(+Input, +OutputType, ?Result) is semidet
+%
+% True if Result is the value of Input converted to OutputType.
+% 
 convert_to_unit(literal(type(InputType, InputVal)), OutputType, Res) :-
-
-    % check if values can be converted at all (equal quantity kind)
-    rdf_has(InputType,  qudt:quantityKind, QKind), % TODO: OR: input float or double
-    rdf_has(OutputType, qudt:quantityKind, QKind),
-
-    % get input multiplier and offset
-    rdf_has(InputType, qudt:conversionMultiplier, literal(type(_,InputConvMult))),
-    rdf_has(InputType, qudt:conversionOffset,     literal(type(_,InputConvOffset))),
-
-    % get output multiplier and offset
-    rdf_has(OutputType, qudt:conversionMultiplier, literal(type(_,OutputConvMult))),
-    rdf_has(OutputType, qudt:conversionOffset,     literal(type(_,OutputConvOffset))),
-
-    term_to_atom(I,  InputVal),
-    term_to_atom(MultI, InputConvMult),
-    term_to_atom(MultO, OutputConvMult),
-    term_to_atom(OffI,  InputConvOffset),
-    term_to_atom(OffO,  OutputConvOffset),
-
-    Res is (((I * MultI + OffI) - OffO) / MultO).
+  % check if values can be converted at all (equal quantity kind)
+  rdf_has(InputType,  qudt:quantityKind, QKind), % TODO: OR: input float or double
+  rdf_has(OutputType, qudt:quantityKind, QKind),
+  % get input multiplier and offset
+  rdf_has_prolog(InputType, qudt:conversionMultiplier, MultI),
+  rdf_has_prolog(InputType, qudt:conversionOffset,     OffI),
+  % get output multiplier and offset
+  rdf_has_prolog(OutputType, qudt:conversionMultiplier, MultO),
+  rdf_has_prolog(OutputType, qudt:conversionOffset,     OffO),
+  term_to_atom(I,  InputVal),
+  Res is (((I * MultI + OffI) - OffO) / MultO).
 
 % default case: input is given as standard xsd type, assume SI base unit
-
 convert_to_unit(Input, OutputType, Res) :-
-
-    (Input = literal(type('http://www.w3.org/2001/XMLSchema#double', InputVal)) ;
-     Input = literal(type('http://www.w3.org/2001/XMLSchema#float',  InputVal)) ),
-
-    % Input multiplier and offset: assume data to be in the SI base unit
-    % corresponding to the OutputType
-    MultI=1.0,
-    OffI=0.0,
-
-    % get output multiplier and offset
-    rdf_has(OutputType, qudt:conversionMultiplier, literal(type(_,OutputConvMult))),
-    rdf_has(OutputType, qudt:conversionOffset,     literal(type(_,OutputConvOffset))),
-
-    term_to_atom(I,  InputVal),
-    term_to_atom(MultO, OutputConvMult),
-    term_to_atom(OffO,  OutputConvOffset),
-
-    Res is (((I * MultI + OffI) - OffO) / MultO).
-
+  (Input = literal(type('http://www.w3.org/2001/XMLSchema#double', InputVal)) ;
+   Input = literal(type('http://www.w3.org/2001/XMLSchema#float',  InputVal)) ),
+  % Input multiplier and offset: assume data to be in the SI base unit
+  % corresponding to the OutputType
+  MultI=1.0,
+  OffI=0.0,
+  % get output multiplier and offset
+  rdf_has_prolog(OutputType, qudt:conversionMultiplier, MultO),
+  rdf_has_prolog(OutputType, qudt:conversionOffset,     OffO),
+  term_to_atom(I,  InputVal),
+  Res is (((I * MultI + OffI) - OffO) / MultO).
