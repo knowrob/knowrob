de87589fb141b93519e2a52619cb9e5dfb942fce
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2018-01-31 20:39:02 +0100

    Removed dummy predicates that were originally added for testing purposes.

--- knowrob_srdl/src/urdf_parser.cpp
+++ knowrob_srdl/src/urdf_parser.cpp
@@ -850,24 +850,3 @@ PREDICATE(joint_safety_kv, 2) {
         return false;
     }
 }
-
-/**************************************/
-/******** DUMMY PREDICATES ************/
-/**************************************/
-
-PREDICATE(foo, 1) {
-    PL_A1 = std::string("foo").c_str();
-    return true;
-}
-
-PREDICATE(bar, 1) {
-    PL_A1 = std::string("bar").c_str();
-    return true;
-}
-
-PREDICATE_NONDET(foo_bar, 1) {
-//    switch( PL_foreign_control(handle) ) {
-//
-//    }
-    return false;
-}
