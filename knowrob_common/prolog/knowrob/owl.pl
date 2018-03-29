2baa2b101fde303a5237db0626e4cfcfd0b0b017
Daniel Beßler <danielb@cs.uni-bremen.de>
2018-03-29 13:11:16 +0200

    removed unused code

--- knowrob_common/prolog/knowrob/owl.pl
+++ knowrob_common/prolog/knowrob/owl.pl
@@ -349,8 +349,6 @@ owl_instance_from_class('http://knowrob.org/kb/knowrob.owl#TimeInterval', I, Tim
 
 owl_instance_from_class('http://knowrob.org/kb/knowrob.owl#Pose', [pose=(Frame,[X,Y,Z],[QW,QX,QY,QZ])], Pose) :- !,
   rdfs_individual_of(Frame, knowrob:'SpatialThing-Localized'),
-  % HACK: there is `object_frame_name/2` but its defined in knowrob_objects
-  rdf_split_url(_, Ref, Frame),
   rdf_unique_id('http://knowrob.org/kb/knowrob.owl#Pose', Pose),
   atomic_list_concat([X,Y,Z], ' ', Translation),
   atomic_list_concat([QW,QX,QY,QZ], ' ', Quaternion),
