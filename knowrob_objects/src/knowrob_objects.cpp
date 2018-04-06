6dc390fd00c57e442facf504c878af3bdfdf8854
Daniel Beßler <danielb@cs.uni-bremen.de>
2018-04-06 21:05:26 +0200

    multi threaded dirty object publisher

--- knowrob_objects/src/knowrob_objects.cpp
+++ knowrob_objects/src/knowrob_objects.cpp
@@ -2,26 +2,88 @@
 
 #include <stdlib.h>
 #include <iostream>
+#include <condition_variable>
+#include <thread>
+#include <chrono>
 
 #include <ros/ros.h>
 #include <knowrob_objects/DirtyObject.h>
 
+class MarkDirtyObjectClient {
+private:
+	std::set<std::string> dirty_objects[2];
+	std::mutex push_lock;
+	std::mutex loop_lock;
+	std::condition_variable loop_cv;
+	std::thread thread;
+	int push_index = 0;
+	bool has_dirty_objects = false;
+	
+public:
+	MarkDirtyObjectClient() : 
+	    thread(&MarkDirtyObjectClient::loop, this)
+	{}
+	
+	void broadcast() {
+		std::set<std::string> &objects = dirty_objects[push_index];
+		if(objects.empty()) return;
+		// ping-pong, less locking
+		push_lock.lock();
+		push_index = (int)!push_index;
+		push_lock.unlock();
+		// fill message
+		knowrob_objects::DirtyObject msg;
+		std::set<std::string>::iterator it;
+		for(it = objects.begin(); it != objects.end(); ++it) {
+			msg.request.object_ids.push_back(*it);
+		}
+		// send message
+		if (!ros::service::call("/object_state_publisher/mark_dirty_object", msg)) {
+			static bool complainedMarkDirtyNotAvailable = false;
+			if(!complainedMarkDirtyNotAvailable) {
+				std::cerr << "Failed to call service " <<
+						"/object_state_publisher/mark_dirty_object. " <<
+						"Is it running?" << std::endl;
+				complainedMarkDirtyNotAvailable = true;
+			}
+		}
+		objects.clear();
+	}
+	
+	void loop() {
+		while(1) {
+			{
+				std::unique_lock<std::mutex> lk(loop_lock);
+				loop_cv.wait(lk, [this]{ return has_dirty_objects; });
+				has_dirty_objects = false;
+			}
+			broadcast();
+			std::this_thread::sleep_for(std::chrono::milliseconds(60));
+		}
+	}
+	
+	void push_begin() { push_lock.lock(); }
+	void push_end()   {
+		push_lock.unlock();
+		{
+			std::lock_guard<std::mutex> lk(loop_lock);
+			has_dirty_objects = true;
+		}
+		loop_cv.notify_one();
+	}
+	
+	void push(const std::string &val) {
+		dirty_objects[push_index].insert(val);
+	}
+};
+
 PREDICATE(mark_dirty_objects, 1) {
-  PlTail tail(PL_A1);
-  PlTerm e;
-  knowrob_objects::DirtyObject msg;
-  while(tail.next(e)) {
-    msg.request.object_ids.push_back(std::string((char*)e));
-  }
-  // TODO: better do this multi threaded, KnowRob spends to much time in here
-  if (!ros::service::call("/object_state_publisher/mark_dirty_object", msg)) {
-    static bool complainedMarkDirtyNotAvailable = false;
-    if(!complainedMarkDirtyNotAvailable) {
-      std::cerr << "Failed to call service " <<
-                "/object_state_publisher/mark_dirty_object. " <<
-                "Is it running?" << std::endl;
-      complainedMarkDirtyNotAvailable = true;
-    }
-  }
-  return TRUE;
+	static MarkDirtyObjectClient mark_dirty_client;
+	PlTail tail(PL_A1); PlTerm e;
+	
+	mark_dirty_client.push_begin();
+	while(tail.next(e)) mark_dirty_client.push(std::string((char*)e));
+	mark_dirty_client.push_end();
+	
+	return TRUE;
 }
