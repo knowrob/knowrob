8aa73684a9e5d4b8ea0e137646ee33906e5b7561
Daniel Beßler <danielb@cs.uni-bremen.de>
2018-04-11 23:38:53 +0200

    fixed marker scale

--- knowrob_vis/prolog/knowrob/marker_vis.pl
+++ knowrob_vis/prolog/knowrob/marker_vis.pl
@@ -311,8 +311,8 @@ marker_initialize_object(Identifier,MarkerObject) :-
   )),
   ignore((
     %not( marker_type(MarkerObject, mesh_resource) ),
-    object_dimensions(Identifier, X, Y, Z),
-    marker_scale(MarkerObject, [X, Y, Z])
+    object_dimensions(Identifier, Depth, Width, Height),
+    marker_scale(MarkerObject, [Width, Depth, Height])
   )),
   ignore((
     object_mesh_path(Identifier, Path),
