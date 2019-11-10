ca4546e2ff04d8abb3791f81e73f2b535ac5d568
Daniel Beßler <danielb@cs.uni-bremen.de>
2019-11-10 22:03:35 +0100

    revised action effect reasoning

--- knowrob_actions/prolog/knowrob/action_effects.pl
+++ knowrob_actions/prolog/knowrob/action_effects.pl
@@ -6,16 +6,7 @@
     ]).
 /** <module> Reasoning about action effects.
 
-KnowRob mainly uses OWL as its knowledge representation language.
-While being very expressive OWL still has some limitations.
-First, action effects may establish new relations between objects.
-We use SWRL to express this (knowrob_common:swrl.pl).
-Second, actions may create/destroy objects. This can further not 
-be expressed in SWRL.
-Here, we use the computable mechanism of KnowRob and a set of Prolog 
-rules that compute processStared, outputCreated, ... relations.
-This has the nice side-effect that we can use computed relations
-in SWRL rules (i.e., relate what was created to something else).
+The effects of actions on objects are derived from the roles the objects play during the action. For example, whenever an object plays the role of being the *AlteredObject* one of its qualities is being altered to some level. Hence the effect of the action is an alteration of the region of the quality which may be automatically updated by KnowRob after the action has been executed. Other distinct cases are objects being destroyed or created in which case KnowRob updates the lifetime of them, and objects being combined, included, or excluded in which case KnowRob may update the compositional relations between these objects.
 
 @author Moritz Tenorth
 @author Daniel Beßler
@@ -24,39 +15,31 @@ in SWRL rules (i.e., relate what was created to something else).
 :- use_module(library('semweb/rdfs')).
 :- use_module(library('semweb/rdf_db')).
 :- use_module(library('semweb/owl')).
-:- use_module(library('swrl')).
-:- use_module(library('swrl_parser')).
 :- use_module(library('knowrob/knowrob')).
-:- use_module(library('knowrob/computable')).
 :- use_module(library('knowrob/action_model')).
 :- use_module(library('knowrob/objects')).
 
 :-  rdf_meta
     action_effect(r,t),
-    action_effects_apply(r,r),
-    comp_processStarted(r,r),
-    comp_outputsCreated(r,r),
-    task_role_range(r,r,r),
-    assign_role_(r,r,r,r).
+    action_effects_apply(r,t).
 
 		 /*******************************
 		 *	Action post-conditions	*
 		 *******************************/
 
-%% action_effect(?Tsk:iri, ?EffectTerm:term) is nondet
+%% action_effect(?Act:iri, ?Effect:term) is nondet
 %
-% Reasoning about which Task has a desired effect.
-% EffectTerm is a Prolog term describing the effect:
+% Relates an action or task to roles that imply chang
+% and to their range.
+% Effect is one of the following terms:
 %
-%	| commited(Type)	| An object has been commited. 	|
-%	| moved(Type)		| An object has been moved.	|
-%	| operated(Type)	| An object has been operated.	|
-%	| removed(Type)		| An object has been removed from a whole.	|
-%	| transformed(Type)	| An object has been transformed.	|
-%	| created(Type)		| An object has been created.	|
-%	| destroyed(Type)	| An object has been destroyed.	|
-%	| started(Type)		| A process has been started.	|
-%	| stopped(Type)		| A process has been stopped.	|
+%	| created(Type)			| An object has been created.	|
+%	| destroyed(Type)		| An object has been destroyed.	|
+%	| altered(Type,QualityType)	| An object has been changed.	|
+%	| combined(Type)		| An object has been combined. 	|
+%	| extracted(Type)		| An object has been extracted.	|
+%	| included(Type)		| An object has been included.	|
+%	| supported(Type)		| An object has been supported.	|
 %
 action_effect(Act,Effect) :-
   action_has_task(Act,Tsk),
@@ -65,114 +48,173 @@ action_effect(Act,Effect) :-
 action_effect(Tsk,Effect) :-
   action_effect_(Tsk,Effect).
 
-action_effect_(Tsk, commited(Type)) :-
-  task_role_range(Tsk,ease_obj:'CommitedObject',Type).
-action_effect_(Tsk, moved(Type)) :-
-  task_role_range(Tsk,ease_obj:'MovedObject',Type).
-action_effect_(Tsk, operated(Type)) :-
-  task_role_range(Tsk,ease_obj:'OperatedObject',Type).
-action_effect_(Tsk, removed(Type)) :-
-  task_role_range(Tsk,ease_obj:'RemovedObject',Type).
-action_effect_(Tsk, transformed(Type)) :-
-  task_role_range(Tsk,ease_obj:'TransformedObject',Type).
 action_effect_(Tsk, created(Type)) :-
   task_role_range(Tsk,ease_obj:'CreatedObject',Type).
 action_effect_(Tsk, destroyed(Type)) :-
   task_role_range(Tsk,ease_obj:'DestroyedObject',Type).
+action_effect_(Tsk, altered(Type,QualityType)) :-
+  task_role_range(Tsk,ease_obj:'AlteredObject',Type),
+  get_altered_quality_type_(Type,QualityType).
+action_effect_(Tsk, combined(Type)) :-
+  task_role_range(Tsk,ease_obj:'CombinedObject',Type).
+action_effect_(Tsk, extracted(Type)) :-
+  task_role_range(Tsk,ease_obj:'ExtractedObject',Type).
+action_effect_(Tsk, included(Type)) :-
+  task_role_range(Tsk,ease_obj:'IncludedObject',Type).
+action_effect_(Tsk, supported(Type)) :-
+  task_role_range(Tsk,ease_obj:'SupportedObject',Type).
 
-action_effect_(Tsk, started(ProcType)) :-
-  property_cardinality(Tsk,ease:starts,ProcType,C,_), C>0,
-  kb_type_of(ProcType,dul:'Process').
-action_effect_(Tsk, stopped(ProcType)) :-
-  property_cardinality(Tsk,ease:finishes,ProcType,C,_), C>0,
-  kb_type_of(ProcType,dul:'Process').
+%% action_effects_apply(+Act,+Grounding) is det.
+%
+% @param Act An individual of type dul:'Action'
+%
+action_effects_apply(Act,Pairs) :-
+  is_list(Pairs), !,
+  dict_pairs(Grounding,_,Pairs),
+  action_effects_apply(Act,Grounding).
 
-%% action_effects_apply(+Act:iri)
-%
-% Apply all the effects that are known to apply if Act is performed successfully.
-%
-% @param Act Instance of knowrob:'Action'
-%
-action_effects_apply(Act,Tsk) :-
-  event_end_time(Act,Stamp),
-  % make sure cached computables are inferred
-  forall(rdfs_computable_triple(knowrob:processStarted, Act, _), true),
-  forall(rdfs_computable_triple(knowrob:outputsCreated, Act, _), true),
-  % assign roles
-  forall(rdf_has(Tsk,dul:isTaskOf,Role), assign_role(Act,Role)),
-  % use swrl for projection of action effects
+action_effects_apply(Act,Grounding0) :-
+  action_has_task(Act,Tsk),
+  dict_pairs(Grounding0,_,Pairs0),
+  % make sure groundings are participants in the action
   forall(
-    ( action_has_task(Act,Tsk), task_effect_rule(Tsk,Rule,Args) ),
-    ( action_effect_rule_project(Act,Rule,Args) ; (
-      print_message(warning, action_effect_failed(Act,Tsk,Args))
-    ))
+    member(_-X0,Pairs0),
+    action_add_filler(Act,X0)
   ),
-  % start/stop lifetime of objects
+  %% instantiate created objects that are not created yet
+  action_create_objects_(Act,Tsk,Grounding0,Pairs1),
+  append(Pairs0,Pairs1,Pairs),
+  dict_pairs(Grounding1,_,Pairs),
+  %% apply effects based on roles of objects
   forall(
-    rdf(Act, knowrob:outputsCreated, Started),
-    object_set_lifetime_begin(Started,Stamp)),
-  forall(
-    rdf(Act, knowrob:inputsDestroyed, Started),
-    object_set_lifetime_end(Started,Stamp)),
-  % start/stop processes
-  forall(
-    rdf(Act, knowrob:processStarted, Started),
-    event_set_begin_time(Started,Stamp)),
-  forall(
-    rdf(Act, knowrob:processStopped, Stopped),
-    event_set_end_time(Stopped,Stamp)),!.
+    get_dict(Concept,Grounding1,Filler),
+    ( object_effects_apply_(Act,Grounding1,Concept,Filler) -> true ; (
+      print_message(warning,
+        runtime_error(cannot_apply_effects_of(Act,Concept)))
+    ))
+  ).
 
 %%
-action_effect_rule_project(Act,Rule,Args) :-
-  swrl_rule_arg_(Rule,Args,action_var,Var),
-  swrl_project(Rule,[var(Var,Act)]).
+object_effects_apply_(_Act,_Grounding,Concept,Filler) :-
+  rdfs_individual_of(Concept,ease_obj:'DestroyedObject'),!,
+  current_time(Now),
+  object_set_lifetime_end(Filler,Now).
 
-%% 
-task_effect_rule(Tsk,Rule,Args) :-
-  rdfs_individual_of(Tsk,Tsk_type),
-  rdf_split_url(_,TskName,Tsk_type),
-  swrl_rule_arg_(Rule,Args,task,TskName).
+object_effects_apply_(_Act,_Grounding,Concept,Filler) :-
+  rdfs_individual_of(Concept,ease_obj:'CreatedObject'),!,
+  current_time(Now),
+  object_set_lifetime_begin(Filler,Now).
 
-swrl_rule_arg_(Rule,Args,Key,Value) :-
-  % HACK
-  swrl_parser:swrl_file_store(_,Rule,Args),
-  get_dict(Key,Args,Value).
+object_effects_apply_(Act,_Grounding,Concept,Filler) :-
+  rdfs_individual_of(Concept,ease_obj:'AlteredObject'),!,
+  get_altered_quality_(Concept,Filler,Quality),
+  kb_triple(Act,ease_obj:hasAlterationResult,Region),
+  quality_set_region_(Quality,Region).
 
-%%
-%assign_role(Act,Role) :-
-  %assign_role_(Act, Role, ease_obj:'CommitedObject', knowrob:?),!.
-assign_role(Act,Role) :-
-  assign_role_(Act,Role, ease_obj:'MovedObject', knowrob:objectTransported),!.
-%assign_role(Act,Role) :-
-  %assign_role_(Act, Role, ease_obj:'OperatedObject', knowrob:?),!.
-assign_role(Act,Role) :-
-  assign_role_(Act, Role, ease_obj:'RemovedObject', knowrob:objectRemoved),!.
-%assign_role(Act,Role) :-
-  %assign_role_(Act, Role, ease_obj:'TransformedObject', knowrob:?),!.
-assign_role(Act,Role) :-
-  assign_role_(Act, Role, ease_obj:'DestroyedObject', knowrob:inputsDestroyed),!.
-assign_role(Act,Role) :-
-  assign_role_(Act, Role, ease_obj:'Tool', knowrob:toolUsed),!.
-assign_role(Act,Role) :-
-  assign_role_(Act, Role, ease_obj:'Destination', knowrob:goalLocation),!.
+object_effects_apply_(_Act,Grounding,Concept,Filler) :-
+  rdfs_individual_of(Concept,ease_obj:'CombinedObject'),!,
+  action_component_system_(Grounding,Parent),
+  object_add_part_(Parent,Filler).
 
-assign_role_(Act,Role,RoleConcept,RoleRelation) :-
-  kb_type_of(Role,RoleConcept),!,
-  forall(kb_triple(Role,dul:classifies,Obj),(
-    kb_retract(Act,dul:hasParticipant,Obj),
-    kb_assert(Act,RoleRelation,Obj)
-  )).
+object_effects_apply_(_Act,Grounding,Concept,Filler) :-
+  rdfs_individual_of(Concept,ease_obj:'ExtractedObject'),!,
+  action_component_system_(Grounding,Parent),
+  % try to remove both part-of and contains relations
+  % as 'Extracted' is not specific here
+  % TODO: use a more specific concept then *Extracted* to distinguish
+  %        between parts and content.
+  object_remove_part_(Parent,Filler),
+  object_remove_content_(Parent,Filler).
+
+object_effects_apply_(_Act,Grounding,Concept,Filler) :-
+  rdfs_individual_of(Concept,ease_obj:'IncludedObject'),!,
+  action_component_system_(Grounding,Parent),
+  object_add_content_(Parent,Filler).
+
+object_effects_apply_(_Act,Grounding,Concept,Filler) :-
+  rdfs_individual_of(Concept,ease_obj:'SupportedObject'),!,
+writeln(supportee(Filler)),
+  action_supporter_(Grounding,Supporter),
+writeln(supporter(Supporter)),
+  object_set_supported_by_(Filler,Supporter).
+
 
 		 /*******************************
-		 *	Computables		*
+		 *	helper		*
 		 *******************************/
 
-%% 
-comp_processStarted(Act,Proc) :-
-  action_effect(Act,started(ProcType)),
-  kb_create(ProcType,Proc).
+%%
+get_altered_quality_type_(Concept,Quality_type) :-
+  rdf_has(Concept,ease_obj:hasAlteredQuality,Quality),
+  kb_type_of(Quality,Quality_type).
 
-%% 
-comp_outputsCreated(Act,Obj) :-
-  action_effect(Act,created(ObjType)),
-  kb_create(ObjType,Obj).
+get_altered_quality_(Concept,Filler,Quality) :-
+  get_altered_quality_type_(Concept,Quality_type),!,
+  object_quality(Filler,Quality_type,Quality).
+
+get_altered_quality_(Concept,Filler,Quality) :-
+  property_range(Concept,ease_obj:hasAlteredQuality,Q0_type),
+  object_quality(Filler,Q0_type,Quality).
+
+%%
+action_supporter_(Grounding,Supporter) :-
+  get_dict(Concept,Grounding,Supporter),
+  rdfs_individual_of(Concept,ease_obj:'Supporter'),!.
+
+%%
+action_component_system_(Grounding,Parent) :-
+  get_dict(Concept,Grounding,Parent),
+  ( rdfs_individual_of(Concept,ease_obj:'CreatedObject');
+    % TODO: there is no good match for the object that incorporated the other one.
+    %       for now using the general *AlteredObject* here but something more specific
+    %       should be used.
+    rdfs_individual_of(Concept,ease_obj:'AlteredObject')
+  ),!.
+
+%%
+action_create_objects_(Act,Tsk,Grounding,Pairs) :-
+  findall(Role-Obj, (
+    kb_triple(Tsk,dul:isTaskOf,Role),
+    once((
+      kb_type_of(Role,ease_obj:'CreatedObject'),
+      property_range(Role,dul:classifies,Type),
+      action_create_object_(Act,Grounding,Type,Obj)
+    ))
+  ), Pairs).
+
+%%
+action_create_object_(Act,Grounding,Type,Obj) :-
+  action_participant(Act,Obj),
+  kb_type_of(Obj,Type),
+  get(Concept,Grounding,Obj),
+  rdfs_individual_of(Concept,ease_obj:'CreatedObject'),!,
+  fail.
+action_create_object_(Act,_Grounding,Type,Obj) :-
+  kb_create(Type,Obj),
+  action_add_filler(Act,Obj).
+
+%%
+quality_set_region_(Quality,Region) :-
+  kb_retract(Quality,dul:hasRegion,_),
+  kb_assert(Quality,dul:hasRegion,Region).
+
+%%
+object_add_part_(Parent,Filler) :-
+  kb_assert(Parent,dul:hasPart,Filler).
+object_remove_part_(Parent,Filler) :-
+  kb_retract(Parent,dul:hasPart,Filler).
+
+%%
+object_add_content_(Parent,Filler) :-
+  kb_assert(Parent,ease_obj:contains,Filler).
+object_remove_content_(Parent,Filler) :-
+  kb_retract(Parent,ease_obj:contains,Filler).
+
+%%
+object_set_supported_by_(Supportee,Supporter) :-
+  % HACK just retract any previous support. Better would
+  %      be to infer this from actions.
+  %      this hack also makes it impossible to be supported 
+  %      multiple times.
+  kb_retract(_,ease_obj:supports,Supportee),
+  kb_assert(Supporter,ease_obj:supports,Supportee).
