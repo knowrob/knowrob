0b7b2aee2ced6f5973abb9d8bc482861f108899f
Daniel Beßler <danielb@cs.uni-bremen.de>
2020-05-13 14:36:02 +0200

    added comments

--- src/comm/notify.pl
+++ src/comm/notify.pl
@@ -1,14 +1,22 @@
 :- module(notify,
     [ notify(t)
     ]).
+/** <module> Broadcasting of notifications.
+
+@author Daniel Beßler
+@license BSD
+*/
 
 :- dynamic notify_hook/1.
 :- multifile notify_hook/1.
 
-%user:term_expansion(
-  %(:-(:(_Module, notify_hook(Term),Goal))),
-  %(:-(:(notify,  notify_hook(Term),Goal)))).
-
+%% notify(+Term) is nondet.
+%
+% Broadcast a notification term.
+% Listeners register by declaring a clause of notify:notify_hook/1.
+%
+% @param Term A notification term.
+%
 notify(Term) :-
   ( notify_hook(Term) *-> true ; true ).
   
