d7068abf38e3e6e81276eaed34ffbad6f1789f0e
Daniel Beßler <danielb@uni-bremen.de>
2020-06-06 21:23:06 +0200

    use subscope_of instead of obsolete scope_satisifes

--- src/lang/terms/occurs.pl
+++ src/lang/terms/occurs.pl
@@ -9,11 +9,10 @@
 
 :- op(1000, xf, occurs).
 
-:- use_module(library('model/DUL/Event'),
-    [ has_time_interval/2 ]).
 :- use_module('../scopes/temporal.pl',
-    [ time_scope_data/2,
-      time_interval_data/3
+    [ time_scope/3,
+      time_scope_data/2,
+      time_subscope_of/2
     ]).
 
 %% occurs(?Event) is nondet.
@@ -23,11 +22,11 @@
 % @param Event an event instance.
 %
 occurs(Evt) ?>
-  query_scope(Query_Scope),
-  { get_dict(time,Query_Scope,Query_TimeScope),
-    time_interval_data(Evt,Since,Until),
-    time_scope(Since,_,Until,Event_TimeScope),
-    time_scope_satisfies(Event_TimeScope,Query_TimeScope)
+  has_interval_data(Evt,Since,Until),
+  query_scope(QScope),
+  { get_dict(time,QScope,QScope_Time),
+    time_scope(=(Since),=(Until),OccursScope),
+    time_subscope_of(OccursScope,QScope_Time)
   }.
 
 occurs(Evt) +>
@@ -40,12 +39,4 @@ occurs(Evt) +>
 occurs1(_,TimeScope)   +> { var(TimeScope),! }.
 occurs1(Evt,TimeScope) +>
   { time_scope_data(TimeScope,[Since,Until]) },
-  { has_time_interval(Evt,Interval) },
-  occurs_since_(Interval,Since),
-  occurs_until_(Interval,Until).
-
-%%
-occurs_since_(_,Since)        +> { var(Since), ! }.
-occurs_since_(Interval,Since) +> holds(Interval,ease:hasIntervalBegin,Since).
-occurs_until_(_,Until)        +> { var(Until), ! }.
-occurs_until_(Interval,Until) +> holds(Interval,ease:hasIntervalEnd,Until).
+  has_interval_data(Evt,Since,Until).
