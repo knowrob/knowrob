8fcb141ee7ace37fc5eab18190bbd34cc51bf3f7
Daniel Beßler <danielb@uni-bremen.de>
2020-06-22 02:34:17 +0200

    added option to run ask queries asynchronously

--- src/lang/query.pl
+++ src/lang/query.pl
@@ -1,10 +1,12 @@
 :- module(lang_query,
-    [ ask(t),     % +Statement
-      ask(t,t),   % +Statement, +Scope
-      tell(t),    % +Statement
-      tell(t,t),  % +Statement, +Scope
-      update(t),  % +Statement
-      update(t,t) % +Statement, +Scope
+    [ ask(t),      % +Statement
+      ask(t,t),    % +Statement, +Scope
+      tell(t),     % +Statement
+      tell(t,t),   % +Statement, +Scope
+      update(t),   % +Statement
+      update(t,t), % +Statement, +Scope
+      is_synchronized(t),  % +Statement
+      synchronize(t)       % +Statement
     ]).
 /** <module> Main interface predicates for querying the knowledge base.
 
@@ -52,6 +54,11 @@ ask(Statement) :-
 % @param Statement a statement term.
 % @param Scope the scope of the statement.
 %
+ask(Statement,[Options,QScope]) :-
+	select_option(callback(CB),Options,Options1),
+	!,
+	ask_asynch(Statement,[Options1,QScope],CB).
+
 ask(Statements,QScope->FScope) :-
   is_list(Statements),!,
   ask_all_(Statements,QScope,_->FScope).
@@ -161,6 +168,138 @@ update(Statement) :-
 update(Statement,Scope0) :-
 	context_update_(Scope0,[options([functional])],Scope1),
 	tell(Statement,Scope1).
+
+		 /*******************************
+		 *	    ASYNCH ASK	     		*
+		 *******************************/
+
+%% A message queue shared between multiple worker threads.
+:- message_queue_create(_,[alias(ask_requests)]).
+:- message_queue_create(_,[alias(ask_queries)]).
+
+%%
+% Maximum number of asynch ask operations running in parallel.
+% Threads are created once, then sleep until ask requests are made.
+%
+num_ask_threads(4).
+
+%%
+% Asynchronously call an ask query without blocking until results
+% are obtained.
+%
+ask_asynch(Query,[Options,QScope],Callback) :-
+	% priority has influence on which request is handled first in case
+	% there are more requests then threads available
+	option(priority(Priority),Options,low),
+	% extract subject from query (it must be first argument)
+	% FIXME: this won't work always, or?
+	Query=..[_,Subject|_],
+	Msg=request(
+		subject(Subject),
+		query(Query,[Options,QScope]),
+		callback(Callback),
+		priority(Priority)
+	),
+	% send the request message to ask_queries and ask_requests queue.
+	% it is removed from ask_requests as soon as a thread picks
+	% the query, and it is removed from ask_queries once the thread
+	% is done.
+	thread_send_message(ask_queries, Msg),
+	thread_send_message(ask_requests,Msg).
+
+%%
+% A worker thread answering queries.
+%
+ask_loop :-
+	repeat,
+	% pop query from queue, this is a blocking call
+	get_request(Msg),
+	% ask a query
+	ask_with_callback(Msg),
+	% pop out Msg from ask_queries queue to indicate that
+	% query processing has finished
+	thread_get_message(ask_queries,Msg),
+	% fallback to 'repeat' above
+	fail.
+
+%%
+ask_with_callback(request(_,query(Query,QScope),callback(Callback),_)) :-
+	catch(
+		ask_with_callback1(Callback,Query,QScope),
+		Exc,
+		print_message(warning, query_exception(Query,Exc))
+	).
+ask_with_callback1(all(Goal),Query,QScope) :-
+	% *all* callbacks are called once with the list of all answers.
+	!,
+	findall([Query,FScope],
+		ask(Query,QScope->FScope),
+		Solutions
+	),
+	list_to_set(Solutions,Set),
+	call(Goal,Set).
+ask_with_callback1(each(Goal),Query,QScope) :-
+	% *each* callbacks are called for each individual answer.
+	!,
+	forall(
+		ask(Query,QScope->FScope),
+		call(Goal,[Query,FScope])
+	).
+ask_with_callback1(one(Goal),Query,QScope) :-
+	% *one* callbacks are called only once for the first answer obtained,
+	% or not at all in case no answer could be found.
+	!,
+	( ask(Query,QScope->FScope)
+	-> call(Goal,[Query,FScope])
+	;  true
+	).
+ask_with_calback1(UnknownTerm,_,_) :-
+	throw(error(syntax_error, ask(callback(UnknownTerm)))).
+
+%%
+get_request(request(S,Q,C,priority(high))) :-
+	% prefer requests with high priority
+	thread_get_message(ask_requests,
+		request(S,Q,C,priority(high)),
+		[timeout(0)]),
+	!.
+get_request(Msg) :-
+	% pick any request
+	thread_get_message(ask_requests,Msg).
+
+%%
+ask_threads_create :-
+	num_ask_threads(Count),
+	forall(
+		between(1,Count,_),
+		thread_create(ask_loop,_)
+	).
+% create threads when this module is consulted
+:- ask_threads_create.
+
+%%
+% True for queries that are not scheduled to be answered by
+% an asynch ask operation, nor are currently being processed.
+%
+is_synchronized(subject(S)) :-
+	!,
+	\+ thread_peek_message(ask_queries, request(subject(S),_,_,_)).
+
+is_synchronized(Q) :-
+	\+ thread_peek_message(ask_queries, request(_,query(Q,_),_,_)).
+
+%%
+% Block as long as a query is queued or being processed in an asynch ask operation.
+%
+synchronize(Term) :-
+	% TODO: get a notification instead of busy waiting
+	% TODO: prefer requests for which synchronize waits,
+	%        they could be added to some queue and handled in get_request
+	repeat,
+	( is_synchronized(Term)
+	-> ( ! )
+	;  ( sleep(0.02), fail )
+	).
 
 		 /*******************************
 		 *	    TERM EXPANSION     		*
@@ -306,3 +445,124 @@ context_update_([Options0,Scope0],New,[Options1,Scope1]) :-
     merge_options(NewOptions,Options0,Options1) ;
     Options1=Options0
   ).
+
+		 /*******************************
+		 *	    UNIT TESTS	     		*
+		 *******************************/
+
+:- begin_tests('lang_query').
+
+:- use_module(library('semweb/rdf_db'),
+		[ rdf_meta/1, rdf_global_term/2 ]).
+
+:- rdf_meta(test_asynch_answer(t)).
+:- rdf_meta(test_asynch_answer0(+,t)).
+
+:- rdf_db:rdf_register_ns(dul,
+		'http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#', [keep(true)]).
+
+% used to get answers from asynch ask
+:- message_queue_create(_,[alias(ask_test_results)]).
+
+test_asynch_finish :-
+	% pop out all messages from ask_test_results queue
+	repeat,
+	\+ thread_get_message(ask_test_results,_,[timeout(0)]),
+	!.
+
+test_asynch_has_finished(Query) :-
+	% an asynch ask test has finished when the query is processed completely,
+	% end all messages were popped out the queue
+	assert_true(is_synchronized(Query)),
+	assert_true(message_queue_property(ask_test_results,size(0))).
+
+test_asynch_ask(Mode,Query) :-
+	% asynch ask test sets the *callback* option and hooks it
+	% to *thread_send_message*
+	current_scope(QScope),
+	Callback=..[Mode,thread_send_message(ask_test_results)],
+	ask(Query, [[callback(Callback)],QScope]).
+
+test_asynch_answer0(Actual,Expected) :-
+	assert_true(is_list(Actual)),
+	assert_true(Actual=[_,_]),
+	Actual=[Fact,FScope],
+	assert_true(ground(Fact)),
+	assert_true(Fact=Expected),
+	assert_true(is_dict(FScope)).
+
+test_asynch_answer(Expected) :-
+	thread_get_message(ask_test_results,Actual),
+	test_asynch_answer0(Actual,Expected).
+
+test('ask asynch all det') :-
+	rdf_global_term(triple(dul:'Object',rdf:type,_),Query),
+	%%
+	test_asynch_ask(all,Query),
+	thread_get_message(ask_test_results,Answers),
+	assert_true(is_list(Answers)),
+	assert_true(length(Answers,1)),
+	Answers=[Answer],
+	test_asynch_answer0(Answer,
+		triple(dul:'Object',rdf:type,owl:'Class')),
+	test_asynch_has_finished(Query).
+
+test('ask asynch one det') :-
+	rdf_global_term(triple(dul:'Object',rdf:type,_),Query),
+	%%
+	test_asynch_ask(one,Query),
+	test_asynch_answer(triple(dul:'Object',rdf:type,owl:'Class')),
+	test_asynch_has_finished(Query).
+
+test('ask asynch each det') :-
+	rdf_global_term(triple(dul:'Object',rdf:type,_),Query),
+	%%
+	test_asynch_ask(each,Query),
+	test_asynch_answer(triple(dul:'Object',rdf:type,owl:'Class')),
+	test_asynch_has_finished(Query).
+
+test('ask asynch all nondet') :-
+	rdf_global_term(triple(_,rdfs:subClassOf,dul:'Object'),Query),
+	%%
+	test_asynch_ask(all,Query),
+	thread_get_message(ask_test_results,Answers),
+	assert_true(is_list(Answers)),
+	assert_true(Answers=[_|_]),
+	findall(X, member([triple(X,_,_),_],Answers), SubClasses),
+	assert_true(SubClasses=[_|_]),
+	assert_true(member(dul:'PhysicalObject',SubClasses)),
+	test_asynch_has_finished(Query).
+
+test('ask asynch one nondet') :-
+	rdf_global_term(triple(dul:'Object',rdfs:subClassOf,_),Query),
+	%%
+	test_asynch_ask(one,Query),
+	test_asynch_answer(Query),
+	test_asynch_has_finished(Query).
+
+test('ask asynch each nondet') :-
+	rdf_global_term(triple(dul:'Object',rdfs:subClassOf,_),Query),
+	%%
+	test_asynch_ask(each,Query),
+	test_asynch_answer(Query),
+	test_asynch_answer(Query),
+	assert_true(synchronize(Query)),
+	assert_true(is_synchronized(Query)),
+	test_asynch_finish.
+
+test('ask asynch multiple queries') :-
+	rdf_global_term(triple(dul:'Object',rdfs:subClassOf,_),Query),
+	QueryCount is 20,
+	% create QueryCount ask requests
+	forall(
+		between(1,QueryCount,_),
+		test_asynch_ask(all,Query)
+	),
+	% poll QueryCount results from message queue
+	forall(
+		between(1,QueryCount,_),
+		thread_get_message(ask_test_results,_)
+	),
+	test_asynch_has_finished(Query).
+
+:- end_tests('lang_query').
