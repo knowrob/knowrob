c7ff3540e889ed73ad0bb56d3336a203bbc9f7c5
Daniel Beßler <danielb@uni-bremen.de>
2020-06-28 12:09:35 +0200

    for now auto-load tf_plugin

--- src/comm/__init__.pl
+++ src/comm/__init__.pl
@@ -1,5 +1,6 @@
 
 :- use_module('notify').
-:- use_module('object_state_publisher').
-% TODO: re-enable ROS.pl
-%:- use_module('ROS').
+
+% TODO should be optional
+:- use_directory('ros/tf').
+%:- use_directory('ros/marker').
