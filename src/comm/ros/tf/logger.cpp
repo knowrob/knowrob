b80d124f008b6f7ba51315257ad35503716461b4
Daniel Beßler <danielb@uni-bremen.de>
2020-06-28 11:55:03 +0200

    TF integration: new plugin for TF logging/querying/publishing

---
+++ src/comm/ros/tf/logger.cpp
@@ -0,0 +1,139 @@
+#include <tf/LinearMath/Quaternion.h>
+
+#include <knowrob/comm/ros/tf/logger.h>
+#include <knowrob/db/mongo/MongoInterface.h>
+
+TFLogger::TFLogger(
+		ros::NodeHandle &node,
+		TFMemory &memory,
+		const std::string &topic) :
+		memory_(memory),
+		timeThreshold_(1.0),
+		vectorialThreshold_(0.1),
+		angularThreshold_(0.1),
+		db_name_("roslog"),
+		topic_(topic),
+		subscriber_(node.subscribe(topic, 1000, &TFLogger::callback, this))
+{
+}
+
+TFLogger::~TFLogger()
+{
+}
+
+void TFLogger::store_document(bson_t *doc)
+{
+	bson_error_t err;
+	MongoCollection *collection = MongoInterface::get_collection(db_name_.c_str(),topic_.c_str());
+	if(!mongoc_collection_insert(
+			(*collection)(),MONGOC_INSERT_NONE,doc,NULL,&err))
+	{
+		ROS_WARN("[TFLogger] insert failed: %s.", err.message);
+	}
+	delete collection;
+}
+
+void TFLogger::store(const geometry_msgs::TransformStamped &ts)
+{
+	bson_t *doc = bson_new();
+	appendTransform(doc, ts);
+	BSON_APPEND_DATE_TIME(doc, "__recorded", time(NULL) * 1000);
+	BSON_APPEND_UTF8(doc, "__topic", topic_.c_str());
+	store_document(doc);
+	bson_destroy(doc);
+}
+
+void TFLogger::callback(const tf::tfMessage::ConstPtr& msg)
+{
+	std::vector<geometry_msgs::TransformStamped>::const_iterator it;
+	for (it = msg->transforms.begin(); it != msg->transforms.end(); ++it)
+	{
+		const geometry_msgs::TransformStamped &ts = *it;
+		if(!ignoreTransform(ts)) store(ts);
+	}
+}
+
+bool TFLogger::ignoreTransform(const geometry_msgs::TransformStamped &ts0)
+{
+	const std::string &child  = ts0.child_frame_id;
+	if(!memory_.has_transform(child)) {
+		// it's a new frame
+		return false;
+	}
+	if(memory_.is_managed_frame(child)) {
+		// managed frames are asserted into DB back-end directly
+		return true;
+	}
+	const geometry_msgs::TransformStamped &ts1 = memory_.get_transform(child);
+	// do not ignore in case parent frame has changed
+	const std::string &parent0 = ts0.header.frame_id;
+	const std::string &parent1 = ts1.header.frame_id;
+	if(parent0.compare(parent1)!=0) {
+		return false;
+	}
+	// test whether temporal distance exceeds threshold
+	if(timeThreshold_>0.0) {
+		double timeDistance = (
+				(ts0.header.stamp.sec * 1000.0 + ts0.header.stamp.nsec / 1000000.0) -
+				(ts1.header.stamp.sec * 1000.0 + ts1.header.stamp.nsec / 1000000.0)) / 1000.0;
+		if(timeDistance<0 || timeDistance > timeThreshold_) {
+			return false;
+		}
+	}
+	// test whether vectorial distance exceeds threshold
+	const geometry_msgs::Vector3 &pos0 = ts0.transform.translation;
+	const geometry_msgs::Vector3 &pos1 = ts1.transform.translation;
+	double vectorialDistance = sqrt(
+			((pos0.x - pos1.x) * (pos0.x - pos1.x)) +
+			((pos0.y - pos1.y) * (pos0.y - pos1.y)) +
+			((pos0.z - pos1.z) * (pos0.z - pos1.z)));
+	if(vectorialDistance > vectorialThreshold_) {
+		return false;
+	}
+	// test whether angular distance exceeds threshold
+	const geometry_msgs::Quaternion &rot0 = ts0.transform.rotation;
+	const geometry_msgs::Quaternion &rot1 = ts1.transform.rotation;
+    tf::Quaternion q1(rot0.x, rot0.y, rot0.z, rot0.w);
+    tf::Quaternion q2(rot1.x, rot1.y, rot1.z, rot1.w);
+    float angularDistance = 2.0 * fabs(q1.angle(q2));
+	if(angularDistance > angularThreshold_) {
+		return false;
+	}
+	return true;
+}
+
+void TFLogger::appendTransform(bson_t *ts_doc, const geometry_msgs::TransformStamped &ts)
+{
+	bson_t child, child2;
+	// append header
+	BSON_APPEND_DOCUMENT_BEGIN(ts_doc, "header", &child); {
+		unsigned long long time = (unsigned long long)(
+				ts.header.stamp.sec * 1000.0 + ts.header.stamp.nsec / 1000000.0);
+		//
+		BSON_APPEND_INT32(&child, "seq", ts.header.seq);
+		BSON_APPEND_DATE_TIME(&child, "stamp", time);
+		BSON_APPEND_UTF8(&child, "frame_id", ts.header.frame_id.c_str());
+		bson_append_document_end(ts_doc, &child);
+	}
+	// append child frame
+	BSON_APPEND_UTF8(ts_doc, "child_frame_id", ts.child_frame_id.c_str());
+	// append transform
+	BSON_APPEND_DOCUMENT_BEGIN(ts_doc, "transform", &child); {
+		// append translation
+		BSON_APPEND_DOCUMENT_BEGIN(&child, "translation", &child2); {
+			BSON_APPEND_DOUBLE(&child2, "x", ts.transform.translation.x);
+			BSON_APPEND_DOUBLE(&child2, "y", ts.transform.translation.y);
+			BSON_APPEND_DOUBLE(&child2, "z", ts.transform.translation.z);
+			bson_append_document_end(&child, &child2);
+		}
+		// append rotation
+		BSON_APPEND_DOCUMENT_BEGIN(&child, "rotation", &child2); {
+			BSON_APPEND_DOUBLE(&child2, "x", ts.transform.rotation.x);
+			BSON_APPEND_DOUBLE(&child2, "y", ts.transform.rotation.y);
+			BSON_APPEND_DOUBLE(&child2, "z", ts.transform.rotation.z);
+			BSON_APPEND_DOUBLE(&child2, "w", ts.transform.rotation.w);
+			bson_append_document_end(&child, &child2);
+		}
+		bson_append_document_end(ts_doc, &child);
+	}
+}
---
+++ src/comm/ros/tf/tf_plugin.plt
@@ -0,0 +1,122 @@
+:- use_module('tf_plugin.pl').
+:- use_module(library('rostest')).
+:- use_module(library('lang/query')).
+:- use_module(library('lang/scopes/temporal')).
+:- use_module(library('db/mongo/client')).
+
+:- use_module(library('lang/terms/is_at'), [ is_at/2 ]).
+:- use_module(library('semweb/rdf_db'), [ rdf_meta/1 ]).
+
+:- begin_tripledb_tests(
+		'tf_plugin',
+		'package://knowrob/owl/test/swrl.owl',
+		[ namespace('http://knowrob.org/kb/swrl_test#'),
+		  setup(tf_mng_whipe),
+		  cleanup(tf_mng_whipe)
+		]).
+
+:- rdf_meta(test_set_pose(r,+,+)).
+:- rdf_meta(test_get_pose(r,+,+)).
+:- rdf_meta(test_trajectory(r,+,+,+)).
+:- rdf_meta(test_transform_pose(r,+,+)).
+:- rdf_meta(test_is_unlocalized(r,+)).
+
+% some test facts
+test_pose_fred0([world,[1.0,0.4,2.32],[0.0,0.0,0.0,1.0]], 1593178679.123).
+test_pose_fred1([world,[2.0,0.4,2.32],[0.0,0.0,0.0,1.0]], 1593178680.123).
+test_pose_fred2([world,[3.0,0.4,2.32],[0.0,0.0,0.0,1.0]], 1593178681.123).
+test_pose_alex1(['Fred',[0.0,1.0,0.0],[0.0,0.0,0.0,1.0]], 1593178680.123).
+
+test_set_pose(Object,Pose,Stamp) :-
+	time_scope(=(Stamp), =<('Infinity'), FScope),
+	assert_true(tf_set_pose(Object,Pose,FScope)).
+
+test_get_pose(Object,Stamp,Expected) :-
+	time_scope(=<(Stamp), >=(Stamp), QScope),
+	assert_true(tf_get_pose(Object,_,QScope,_)),
+	( tf_get_pose(Object,Actual,QScope,_)
+	-> assert_unifies(Actual,Expected)
+	;  true
+	).
+
+test_trajectory(Obj,Begin,End,Expected) :-
+	assert_true(tf_get_trajectory(Obj,Begin,End,_)),
+	( tf_get_trajectory(Obj,Begin,End,Actual)
+	-> assert_unifies(Actual,Expected)
+	;  true
+	).
+
+test_transform_pose(Object,Stamp,Query) :-
+	time_scope(=<(Stamp), >=(Stamp), QScope),
+	assert_true(tf_get_pose(Object,Query,QScope,_)).
+
+test_lookup(Frame,Stamp,Expected) :-
+	assert_true(tf_mng_lookup(Frame,Stamp,Stamp,_,_,_)),
+	( tf_mng_lookup(Frame,Stamp,Stamp,Actual,_,_)
+	-> assert_unifies(Actual,Expected)
+	;  true
+	).
+
+test_lookup_fails(Frame,Stamp) :-
+	assert_false(tf_mng_lookup(Frame,Stamp,Stamp,_,_,_)).
+
+test_is_unlocalized(Object,Stamp) :-
+	time_scope(=<(Stamp), >=(Stamp), QScope),
+	assert_false(tf_get_pose(Object,_,QScope,_)).
+
+test('tf_pose') :-
+	test_pose_fred0(Pose0,Stamp0),
+	Past   is Stamp0 - 1.0,
+	Future is Stamp0 + 1.0,
+	%%
+	test_is_unlocalized(test:'Fred',Stamp0),
+	test_set_pose(test:'Fred',Pose0,Stamp0),
+	test_get_pose(test:'Fred',Stamp0,Pose0),
+	test_get_pose(test:'Fred',Future,Pose0),
+	test_is_unlocalized(test:'Fred',Past).
+
+test('tf_mongo_lookup') :-
+	test_pose_fred0(Pose0,Stamp0),
+	Past is Stamp0 - 1.0,
+	test_lookup('Fred',Stamp0,Pose0),
+	test_lookup_fails('Fred',Past).
+
+test('tf_pose_memory') :-
+	test_pose_fred0(Pose0,Stamp0),
+	test_pose_fred1(Pose1,Stamp1),
+	test_pose_fred2(Pose2,Stamp2),
+	Stamp01 is 0.5*(Stamp0 + Stamp1),
+	Future is Stamp2 + 1.0,
+	%%
+	test_set_pose(test:'Fred',Pose1,Stamp1),
+	test_set_pose(test:'Fred',Pose2,Stamp2),
+	%%
+	test_get_pose(test:'Fred',Stamp0,Pose0),
+	test_get_pose(test:'Fred',Stamp01,Pose0),
+	test_get_pose(test:'Fred',Stamp1,Pose1),
+	test_get_pose(test:'Fred',Stamp2,Pose2),
+	test_get_pose(test:'Fred',Future,Pose2).
+
+test('tf_trajectory') :-
+	test_pose_fred0(Pose0,Stamp0),
+	test_pose_fred1(Pose1,Stamp1),
+	test_pose_fred2(Pose2,Stamp2),
+	test_trajectory(test:'Fred',Stamp0,Stamp2,
+		[Stamp0-Pose0, Stamp1-Pose1, Stamp2-Pose2]),
+	test_trajectory(test:'Fred',Stamp1,Stamp2,
+		[Stamp1-Pose1, Stamp2-Pose2]).
+
+test('tf_transform_pose') :-
+	test_pose_alex1(Pose1,Stamp1),
+	test_set_pose(test:'Alex',Pose1,Stamp1),
+	test_get_pose(test:'Alex',Stamp1,Pose1),
+	test_transform_pose(test:'Alex',Stamp1,[world,[2.0,1.4,2.32],_]),
+	test_transform_pose(test:'Fred',Stamp1,['Alex',_,_]).
+
+test('tf_is_at') :-
+	test_pose_fred0(Pose0,Stamp0),
+	assert_true(ask(
+		during(is_at(test:'Fred',Pose0), [Stamp0,Stamp0])
+	)).
+
+:- end_tests('tf_plugin').
