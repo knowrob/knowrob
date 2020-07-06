3894f25bbb55d5df7e7de67ae747652320eb4789
kaviyachandran <kaviya.dhanabalachandran@gmail.com>
2020-07-06 18:07:17 +0200

    Add test cases for lang_occur module

--- src/lang/terms/occurs.pl
+++ src/lang/terms/occurs.pl
@@ -49,3 +49,27 @@ occurs1(_,TimeScope)   +> { var(TimeScope),! }.
 occurs1(Evt,TimeScope) +>
   { time_scope_data(TimeScope,[Since,Until]) },
   has_interval_data(Evt,Since,Until).
+
+     /*******************************
+     *        UNIT TESTS            *
+     *******************************/
+
+:- begin_tripledb_tests(
+    lang_occurs,
+    'package://knowrob/owl/test/events.owl',
+    [ namespace('http://knowrob.org/kb/test_events.owl#')
+    ]).
+
+test('tell and ask event occurs during a time interval') :-
+  assert_true(occurs(test:'Short4') during [1377777009, 1377777011]),
+  assert_true(occurs(test:'Short1') during test:'Time_Short1'),
+  assert_false(occurs(test:'Event6') during test:'Time_Long'),
+  assert_true(tell(occurs(test:'Event6') during test:'Time_Long')),
+  assert_true(occurs(test:'Event6') during test:'Time_Long').
+
+test('tell and ask an event occurs during an event') :-
+  assert_true(tell(holds(test:'Event6',dul:'hasTimeInterval', test:'Time_Long'))),
+  assert_true(tell(occurs(test:'Event0') during test:'Event6')),
+  assert_true(occurs(test:'Event0') during test:'Event6').
+
+:- end_tripledb_tests(lang_occurs).
