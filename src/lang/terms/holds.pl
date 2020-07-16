69329ec866c8f4d331a6ea9069e26387239a5363
Daniel Beßler <danielb@uni-bremen.de>
2020-07-16 19:12:47 +0200

    write list term in argument to avoid is_ist call

--- src/lang/terms/holds.pl
+++ src/lang/terms/holds.pl
@@ -52,9 +52,9 @@ holds(Query) ?>
 % @param Property The predicate of a triple.
 % @param Value The object of a triple.
 %
-holds(S,Chain,O) ?>
-  { is_list(Chain),! },
-  holds_chain(S,Chain,O).
+holds(S,[P|Ps],O) ?>
+  { ! },
+  holds_chain(S,[P|Ps],O).
 
 holds(S,P,O) ?>
   { once((var(P); is_object_property(P))) },
