76b9bf65eb52aad2676e6e5679c3eec3585e582d
Daniel Beßler <danielb@uni-bremen.de>
2020-07-16 19:18:30 +0200

    asynch notify

--- src/comm/notify.pl
+++ src/comm/notify.pl
@@ -1,5 +1,6 @@
 :- module(notify,
-    [ notify(t)
+    [ notify(t),
+      notify_synchronize(t)
     ]).
 /** <module> Broadcasting of notifications.
 
@@ -8,7 +9,37 @@
 */
 
 :- dynamic notify_hook/1.
+:- dynamic notify_thread/1.
 :- multifile notify_hook/1.
+
+%%
+% A worker thread processing notify messages.
+%
+notify_loop :-
+	repeat,
+	thread_get_message(Msg),
+	catch(
+		forall(notify_hook(Msg),true),
+		Exception,
+		print_message(error,notify_exception(Exception))
+	),
+	% fallback to 'repeat' above
+	fail.
+
+% create threads when this module is consulted
+:- thread_create(notify_loop,Thread),
+   assertz(notify_thread(Thread)).
+
+%% notify_synch(+Term) is nondet.
+%
+notify_synchronize(Term) :-
+	notify_thread(Thread),
+	repeat,
+	(	thread_peek_message(Thread,Term)
+	->	( sleep(0.01), fail )
+	;	true
+	),
+	!.
 
 %% notify(+Term) is nondet.
 %
@@ -17,6 +48,13 @@
 %
 % @param Term A notification term.
 %
+notify(individual(X)) :-
+	!,
+	(	is_object(X) -> notify(object(X))
+	;	is_event(X)  -> notify(event(X))
+	;	true
+	).
+
 notify(Term) :-
-  ( notify_hook(Term) *-> true ; true ).
-  
+	notify_thread(Thread),
+	thread_send_message(Thread, Term).
