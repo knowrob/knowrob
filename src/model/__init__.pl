2ccb5260bd67845aed7fbaa1b315c068fadc683c
Daniel Beßler <danielb@cs.uni-bremen.de>
2020-08-20 12:11:01 +0200

    auto load urdf

--- src/model/__init__.pl
+++ src/model/__init__.pl
@@ -9,7 +9,7 @@
 % load init files in sub-directories
 :- use_directory('DUL').
 :- use_directory('EASE').
-%:- use_directory('URDF').
+:- use_directory('URDF').
 
 % load additional ontologies
 :- tripledb_load(
