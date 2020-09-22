c5ee204434436e41ad27bef3ce534e848d1ee521
Daniel Beßler <danielb@uni-bremen.de>
2020-09-22 19:46:00 +0200

    fix for read-only db

--- src/db/tripledb.pl
+++ src/db/tripledb.pl
@@ -176,10 +176,7 @@ tripledb_load(URL,Options) :-
   ),
   % get fact scope
   universal_scope(Scope),
-  ( setting(mng_client:read_only, true) 
-  	->  true
-  	; tripledb_load(URL,Scope,Graph)
-  ).
+  tripledb_load(URL,Scope,Graph).
 
 %% tripledb_load(+URL,+Scope,+Graph) is semidet.
 %
@@ -212,7 +209,10 @@ tripledb_load(URL,Scope,SubGraph) :-
 	),
 	!,
 	%%
-	tripledb_load0(Resolved,Scope,OntoGraph,SubGraph).
+	(	setting(mng_client:read_only, true)
+	->	true
+	;	tripledb_load0(Resolved,Scope,OntoGraph,SubGraph)
+	).
 
 tripledb_load0(Resolved,_,OntoGraph,_) :-
 	% test whether the ontology is already loaded
