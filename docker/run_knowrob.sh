a6296a525a140523be507f157f2b9533bb0648eb
Daniel Beßler <danielb@uni-bremen.de>
2020-11-22 11:11:19 +0100

    add another package path at /openease_ws

--- docker/run_knowrob.sh
+++ docker/run_knowrob.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+source /home/ros/devel/setup.bash
+export ROS_PACKAGE_PATH="$ROS_PACKAGE_PATH:/home/ros/user_data/:/openease_ws"
+
 roslaunch /home/ros/src/knowrob/launch/openease.launch
