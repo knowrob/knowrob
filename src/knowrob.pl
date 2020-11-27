eb6870f6b31d31bfc483ee04e805d91aa06bba65
Daniel Beßler <danielb@uni-bremen.de>
2020-11-27 15:27:13 +0100

    neem_init

--- src/knowrob.pl
+++ src/knowrob.pl
@@ -1,12 +1,32 @@
 :- module(knowrob,
 	[ knowrob_load_settings/0,
-	  knowrob_load_plugins/0
+	  knowrob_load_plugins/0,
+	  neem_init0/1
 	]).
 
 :- use_module(library(settings)).
 
 % define some settings
 :- setting(plugins, list, [], 'List of auto-loaded plugins').
+
+neem_init0(NEEM_id) :-
+	% assign DB collection prefix
+	set_setting(mng_client:collection_prefix, NEEM_id),
+	% load URDF files referred to in triple store
+	urdf_init,
+	% initialize position of each frame for tf publishing
+	tf_tree:initial_transforms(InitialTransforms),
+	forall(
+	    (   member([Ref,Frame,Pos,Rot],InitialTransforms),
+	        % FIXME avoid this elsewhere
+	        Ref \= Frame,
+	        \+ atom_concat('/',Ref,Frame),
+	        \+ atom_concat('/',Frame,Ref)
+	    ),
+		tf_plugin:tf_republish_set_pose(Frame,[Ref,Pos,Rot])
+	),
+	% publish object marker messages
+	marker_plugin:republish.
 
 %% restore_settings is det.
 %
