8fa71656f9e16f86b6547e8bd880af6cd6ae43dc
Daniel Beßler <danielb@uni-bremen.de>
2021-01-17 11:05:00 +0100

    added option to run cursor and ignore that it has no results

--- src/db/mongo/mongo_kb.cpp
+++ src/db/mongo/mongo_kb.cpp
@@ -174,3 +174,14 @@ PREDICATE(mng_cursor_next_json, 2) {
 		return FALSE;
 	}
 }
+
+PREDICATE(mng_cursor_run, 1) {
+	char* cursor_id = (char*)PL_A1;
+	const bson_t *doc;
+	if(MongoInterface::cursor(cursor_id)->next(&doc,true)) {
+		return TRUE;
+	}
+	else {
+		return FALSE;
+	}
+}
