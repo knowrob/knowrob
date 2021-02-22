6e34fb3f055555cc96f4d82c3c7779a9c8a5c7c6
Daniel Beßler <danielb@uni-bremen.de>
2021-02-22 20:09:56 +0100

    watch docu

--- src/lang/db.pl
+++ src/lang/db.pl
@@ -442,28 +442,39 @@ version_matcher --> digits(_), ".", digits(_).
      *          DB EVENTS           *
      *******************************/
 
+%% watcher(+WatcherID, +Goal)
+%
+% Stores information about active watch processes.
+%
 :- dynamic watcher/2.
 
 %% watch(+Goal, +Callback, -WatcherID) is semidet.
 %
 % Start watching possible instantiations of variables in Goal.
+% Goal must be a KnowRob language term.
 % Callback is called whenever the set of possible instantiations changes,
 % and it is provided with information about the change.
 % The change information is encoded in a term that is appended to
 % already existing arguments of Callback, if any.
 %
-% NOTE: currently Goal must be a term triple/3. No other language terms
-%       are supported yet.
-% FIXME: callback is currently not called when documents are removed!
+% Note that callback is currently not called when documents are removed!
+% This might change in the future.
+%
+% Also note that currently Goal must be a term triple/3.
+% The scope of triples is ignored.
+% No other predicates are supported yet.
+% This might also change in the future.
+%
+% @Goal a KnowRob language term with free variables.
+% @Callback a predicate called for each change event.
+% @WatcherID a unique identifier of the watching operation.
 %
 watch(Goal, Callback, WatcherID) :-
 	% create match filter based on Goal
 	% NOTE: it is not possible to $match remove events like this
 	%       because the document values are _not_ included in the event :/
-	%       this means additional bookeeping is needed to be able to handle
-	%       remove events (e.g. to add additional watcher on document ID of
-	%       documents returned by watch before), and then the watcher would
-	%       need to be able to handle document IDs.
+	%       this means additional bookkeeping is needed to be able to handle
+	%       remove events ...
 	watch_filter(Goal, DB, Coll, WatchFilter),
 	% start receiving events about documents that match the filter
 	mng_watch(DB, Coll,
@@ -476,18 +487,27 @@ watch(Goal, Callback, WatcherID) :-
 	%        before this fact is asserted
 	assertz(watcher(WatcherID, Callback)).
 
+%% unwatch(+WatcherID) is semidet.
+%
+% Stop a previously started watch operation.
+% This will fail if WatcherID does not belong to
+% a active watch operation.
+%
+% @WatcherID a unique identifier of the watching operation.
+%
 unwatch(WatcherID) :-
 	once(watcher(WatcherID, _)),
 	mng_unwatch(WatcherID),
 	retractall(watcher(WatcherID, _)).
 
-%% watch_event(+WatcherID, +Event)
+%% watch_event(+WatcherID, +Event) is semidet.
 %
+% This predicate is called by the mongo client when
+% changes are received in a watch operation.
 % Event is a change stream response document.
-% The field "operationType" must be one of:
-%  insert, delete, replace, update, drop, rename, dropDatabase, or invalidate.
-% For insert, replace, and update, the "fullDocument" field holds the
-% modified document.
+%
+% @WatcherID a unique identifier of the watching operation.
+% @Event a change stream output document.
 %
 % @see https://docs.mongodb.com/manual/reference/change-events/#change-stream-output
 %
@@ -496,29 +516,35 @@ watch_event(WatcherID, Event) :-
 	dict_pairs(Dict, _, Event),
 	mng_get_dict(operationType, Dict, string(OpType)),
 	memberchk(OpType, [insert, update]),
-	% read the triple document from "fullDocument" field
+	% read the document from "fullDocument" field
 	mng_get_dict(fullDocument,  Dict, FullDoc),
-	mng_get_dict('_id', FullDoc, DocID),
+	mng_get_dict('_id', FullDoc, id(DocID)),
 	% parse triple
+	% TODO: support other terms here
 	mng_get_dict(s, FullDoc, string(S)),
 	mng_get_dict(p, FullDoc, string(P)),
 	mng_get_dict(o, FullDoc, TypedValue),
 	mng_strip_type(TypedValue, _, Value),
 	% finally call the watcher
-	UpdateTerm = event(OpType, DocID, [
+	UpdateTerm = event(OpType, [
+		id(DocID),
 		new_value(triple(S,P,Value))
 	]),
 	catch(
 		call(CallbackGoal, UpdateTerm),
 		Exc,
-		log_error_and_fail(lang(Exc, watch_callback(CallbackGoal, UpdateTerm)))
+		log_error_and_fail(lang(Exc,
+			watch_callback(CallbackGoal, UpdateTerm)))
 	).
 
+%%
 watch_filter(triple(S,P,V), DB, Coll, WatchFilter) :-
 	% TODO: howto handle scope of facts here?
+	% TODO: support other terms here
 	mng_get_db(DB, Coll, 'triples'),
 	lang_triple:mng_triple_doc(triple(S,P,V), Doc, []),
-	% the event holds the updated document in a field "fullDocument" 
+	% the event holds the updated document in a field "fullDocument"
+	% NOTE: not for remove events!
 	maplist([[Key0,Val],[Key1,Val]]>>
 		atom_concat('fullDocument.', Key0, Key1),
 		Doc, WatchFilter).
