356862bf8d4b71ecedc89698fcff92fd2568c61e
Daniel Beßler <danielb@uni-bremen.de>
2021-03-14 16:15:23 +0100

    cleanup

--- src/ros/tf/tf_mongo.pl
+++ src/ros/tf/tf_mongo.pl
@@ -2,7 +2,6 @@
 	[ tf_mng_store/3,
 	  tf_mng_lookup/6,
 	  tf_mng_trajectory/4,
-	  tf_mng_range/6,
 	  tf_mng_drop/0
 	]).
 
@@ -90,86 +89,3 @@ tf_mng_lookup(
 		double(Fact_Since),
 		double(Fact_Until),
 		FactScope).
-
-%tf_mng_lookup(ObjFrame,QSince,QUntil,PoseData,FSince,FUntil) :-
-%	mng_db_name(DB),
-%	tf_db(DB, Name),
-%	mng_cursor_create(DB,Name,Cursor),
-%	mng_cursor_descending(Cursor,'header.stamp'),
-%	mng_cursor_filter(Cursor, ['child_frame_id', string(ObjFrame)]),
-%	mng_cursor_filter(Cursor, ['header.stamp', ['$lte', time(QUntil)]]),
-%	%mng_cursor_filter(Cursor, ['header.stamp', ['$gte', time(Stamp0)]]),
-%	setup_call_cleanup(
-%		true,
-%		tf_mng_lookup1(Cursor,QSince,QUntil,_,PoseData,FSince,FUntil),
-%		mng_cursor_destroy(Cursor)
-%	).
-
-tf_mng_range(QSince,QUntil,ObjFrame,PoseData,FSince,FUntil) :-
-	mng_db_name(DB),
-	tf_db(DB, Name),
-	mng_cursor_create(DB,Name,Cursor),
-	mng_cursor_filter(Cursor, ['header.stamp', ['$lt', time(QUntil)]]),
-	mng_cursor_filter(Cursor, ['header.stamp', ['$gte', time(QSince)]]),
-	mng_cursor_descending(Cursor,'header.stamp'),
-	setup_call_cleanup(
-		true,
-		tf_mng_lookup1(Cursor,QSince,QUntil,ObjFrame,PoseData,FSince,FUntil),
-		mng_cursor_destroy(Cursor)
-	),
-	% TODO: read frame name
-	true.
-
-tf_mng_lookup1(Cursor,MinStamp,MaxStamp,ObjFrame,PoseData,FSince,FUntil) :-
-	mng_cursor_next(Cursor,First),
-	mng_get_dict(header,First,Header),
-	mng_get_dict(stamp,Header,double(FirstStamp)),
-	( FirstStamp > MaxStamp
-	->	( FirstUntil=FirstStamp,
-	      mng_cursor_next(Cursor,Doc)
-		)
-	;	( FirstUntil='Infinity',
-		  Doc=First
-		)
-	),
-	tf_mng_lookup2(Cursor,Doc,MinStamp,FirstUntil,ObjFrame,PoseData,FSince,FUntil).
-
-tf_mng_lookup2(Cursor,Next,MinStamp,LastStamp,ObjFrame,PoseData,FSince,FUntil) :-
-	tf_mng_doc_pose(Next,_,Stamp,PoseData0),
-	(	( PoseData=PoseData0,
-		  FSince=Stamp,
-		  FUntil=LastStamp,
-		  mng_get_dict(child_frame_id,Next,ObjFrame)
-		)
-	;	( Stamp > MinStamp,
-		  mng_cursor_next(Cursor,X),
-		  tf_mng_lookup2(Cursor,X,MinStamp,Stamp,ObjFrame,PoseData,FSince,FUntil)
-		)
-	).
-
-%%
-% Convert mongo document to pose term.
-%
-tf_mng_doc_pose(Doc,ObjFrame,Time,[ParentFrame,[TX,TY,TZ],[QX,QY,QZ,QW]]) :-
-	get_dict(child_frame_id,Doc,
-		string(ObjFrame)
-	),
-	get_dict(header,Doc,[
-		_,
-		stamp-double(Time),
-		frame_id-string(ParentFrame)
-	]),
-	get_dict(transform,Doc,[
-		translation-[
-			x-double(TX),
-			y-double(TY),
-			z-double(TZ)
-		],
-		rotation-[
-			x-double(QX),
-			y-double(QY),
-			z-double(QZ),
-			w-double(QW)
-		]
-	]).
-
