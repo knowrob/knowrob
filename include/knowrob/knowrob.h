0eaa4d540a22aeb9e6dcec08995a7078c0c78346
danielb <danielb@uni-bremen.de>
2023-01-04 22:54:42 +0100

    +InitKnowledgeBase to keep track of argv[0]

---
+++ include/knowrob/knowrob.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023, Daniel Beßler
+ * All rights reserved.
+ *
+ * This file is part of KnowRob, please consult
+ * https://github.com/knowrob/knowrob for license details.
+ */
+
+#ifndef __KNOWROB_H__
+#define __KNOWROB_H__
+
+namespace knowrob {
+	void InitKnowledgeBase(int argc, char **argv);
+
+	char* getNameOfExecutable();
+}
+
+#endif //__KNOWROB_H__
---
+++ src/knowrob.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023, Daniel Beßler
+ * All rights reserved.
+ *
+ * This file is part of KnowRob, please consult
+ * https://github.com/knowrob/knowrob for license details.
+ */
+
+#include <knowrob/knowrob.h>
+#include <knowrob/logging.h>
+
+namespace knowrob {
+	// stores the name of the executable as provided in argv[0]
+	char *NAME_OF_EXECUTABLE = nullptr;
+
+	char* getNameOfExecutable()
+	{
+		if(NAME_OF_EXECUTABLE) {
+			return knowrob::NAME_OF_EXECUTABLE;
+		} else {
+			static char noExec[] = "<no-executable>";
+			return noExec;
+		}
+	}
+
+	void InitKnowledgeBase(int argc, char **argv)
+	{
+		// remember the program name.
+		// it is assumed here that argv stays valid during program execution.
+		knowrob::NAME_OF_EXECUTABLE = argv[0];
+		// configure the logger
+		logging::initialize();
+	}
+}
