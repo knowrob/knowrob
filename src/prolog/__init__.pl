7bbbf023862c8043c71b0a53dd4e1ae2b939d666
danielb <danielb@uni-bremen.de>
2023-01-10 16:26:51 +0100

    load blackboard.pl

--- src/prolog/__init__.pl
+++ src/prolog/__init__.pl
@@ -1,6 +1,7 @@
 
-% this init file is intended to be loaded by the
-% PrologReasoner as soon as it starts.
+% this is a global initialization for Prolog-based
+% reasoning in KnowRob. Several reasoner instances
+% are able to access what is imported here.
 
 % configure how Prolog prints terms
 :- set_prolog_flag(toplevel_print_anon, false).
@@ -13,7 +14,6 @@
 :- set_prolog_flag(float_format, '%.12g').
 
 % rules may generate discontiguous clauses.
-% TODO: find a better solution then disabling this globally.
 %:- style_check(-discontiguous).
 
 % add the toplevel src directory as Prolog library_directory
@@ -22,28 +22,21 @@
    asserta(user:library_directory(PrologDir)),
    asserta(user:library_directory(SrcDir)).
 
-% load some standard Prolog libraries into user
+% load common Prolog libraries
 :- use_module(library('semweb/rdf_db'), [rdf_meta/1, rdf_current_ns/2, rdf_register_prefix/3]).
-
-% register some custom RDF namespaces
+% register some common RDF namespaces
 :- rdf_register_prefix(dul,  'http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#', [keep(true)]).
 :- rdf_register_prefix(soma, 'http://www.ease-crc.org/ont/SOMA.owl#', [keep(true)]).
 
 % more fancy module declarations
 :- use_module(library('module')).
-
 % message formatting and logging
 :- use_module(library('messages')).
 :- use_module(library('logging')).
-
 % tooling around plunit
 :- use_module(library('unittest')).
 
-% TODO Load settings from file.
-%:- ( getenv('KNOWROB_SETTINGS', File)
-%	-> load_settings(File,[undefined(load)])
-%	;  true
-%	).
+:- dynamic user:defined_reasoner_setting/4.
 
 :- use_module(library('filesystem')).
 :- use_module(library('functional')).
@@ -51,7 +44,14 @@
 :- use_module(library('url')).
 %:- use_module(library('atom')).
 
-% auto-loaded models
+% extensions for semantic web
+:- use_module(library('semweb')).
+:- sw_add_subgraph(user,common),
+   sw_add_subgraph(test,user).
+
+% auto-load common models
 :- use_module(library('xsd')).
 :- use_module(library('qudt')).
-:- use_module(library('semweb_ext')).
+
+% predicates that interact with the QA system in some way
+:- use_module(library('blackboard')).
