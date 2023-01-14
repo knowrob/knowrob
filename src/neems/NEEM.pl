6ee3f605f29668a95d1569050ecdb66a8f560585
danielb <danielb@uni-bremen.de>
2023-01-14 10:39:58 +0100

    updated tests to use mongolog predicates directly

--- src/neems/NEEM.pl
+++ src/neems/NEEM.pl
@@ -1,4 +1,4 @@
-:- module(model_NEEM,
+:- module(mongolog_neems,
     [ knowrob_load_neem/1
     ]).
 
@@ -32,33 +32,40 @@ knowrob_load_neem(NEEM_id) :-
 	% publish object marker messages
 	marker:republish.
 
-:- use_module(library('semweb/rdf_db'), [ rdf_equal/2 ]).
-:- use_module('terms').
- 
-:- begin_rdf_tests(model_NEEM,
+:- use_module(library('mongolog/mongolog_test')).
+:- begin_mongolog_tests(mongolog_neems,
 		'owl/test/memory.owl',
 		[ namespace('http://knowrob.org/kb/mem-test.owl#') ]).
+
+:- use_module(library('semweb/rdf_db'), [ rdf_equal/2 ]).
+:- use_module(library('mongolog/mongolog'), [ mongolog_call/1 ]).
+:- use_module(library('scope')).
+:- use_module('terms').
+:- use_module('DUL').
+:- use_module('SOMA').
 
 :- dynamic test_episode/1,
            test_action/1.
 
 test('is_episode') :-
-	kb_call(new_iri(Episode,dul:'Situation')),
-	kb_project(is_episode(Episode)),
+	universal_scope(Scope),
+	mongolog_call(new_iri(Episode,dul:'Situation')),
+	mongolog_call(project(is_episode(Episode)), [scope(Scope)]),
 	assert_true(ground(Episode)),
-	assert_true(is_episode(Episode)),
+	assert_true(mongolog_call(is_episode(Episode))),
 	assertz(test_episode(Episode)).
 
 test('is_setting_for') :-
+	universal_scope(Scope),
 	test_episode(Episode),
 	%% create an action
-	kb_call(new_iri(Action,dul:'Action')),
-	kb_project(is_action(Action)),
+	mongolog_call(new_iri(Action,dul:'Action')),
+	mongolog_call(project(is_action(Action)), [scope(Scope)]),
 	assert_true(ground(Action)),
 	assert_true(is_action(Action)),
 	%% assert is_setting_for
 	assert_false(is_setting_for(Episode,Action)),
-	assert_true(kb_project(is_setting_for(Episode,Action))),
+	assert_true(mongolog_call(project(is_setting_for(Episode,Action)), [scope(Scope)])),
 	assert_true(is_setting_for(Episode,Action)),
 	%%
 	assertz(test_action(Action)).
@@ -66,83 +73,74 @@ test('is_setting_for') :-
 test('occurs') :-
 	test_action(Action),
 	assert_false(occurs(Action) during [24,464]),
-	assert_true(kb_project(occurs(Action) during [24,464])),
+	assert_true(mongolog_call(project(occurs(Action) during [24,464]))),
 	assert_true(occurs(Action) during [24,464]).
 
 test('executes_task') :-
+	universal_scope(Scope),
 	test_action(Action),
 	%% state what task the action executes
-	kb_call(new_iri(Task,test:'TestTask')),
-	kb_project(has_type(Task,test:'TestTask')),
+	mongolog_call(new_iri(Task,test:'TestTask')),
+	mongolog_call(project(has_type(Task,test:'TestTask')), [scope(Scope)]),
 	assert_true(ground(Task)),
+
+	semweb:sw_get_subgraphs(test,Subs),
+	writeln(executes_task_SUBS_XXXXXXXX(Subs)),
+
 	assert_true(is_task(Task)),
 	%%
 	assert_false(executes_task(Action,Task)),
-	assert_true(kb_project(executes_task(Action,Task))),
+	assert_true(mongolog_call(project(executes_task(Action,Task)), [scope(Scope)])),
 	assert_true(executes_task(Action,Task)).
 
 test('has_participant') :-
+	universal_scope(Scope),
 	test_action(Action),
 	rdf_equal(test:'Substance_0',Obj),
 	%%
 	assert_false(has_participant(Action,Obj)),
-	assert_true(kb_project(has_participant(Action,Obj))),
+	assert_true(mongolog_call(project(has_participant(Action,Obj)), [scope(Scope)])),
 	assert_true(has_participant(Action,Obj)).
 
 test('has_role') :-
+	universal_scope(Scope),
 	test_action(Action),
 	rdf_equal(test:'Substance_0',Obj),
 	%%
-	kb_call(new_iri(Role,test:'ARole')),
-	kb_project(has_type(Role,test:'ARole')),
+	mongolog_call(new_iri(Role,test:'ARole')),
+	mongolog_call(project(has_type(Role,test:'ARole')), [scope(Scope)]),
 	assert_false(has_role(Obj,Role) during Action),
-	assert_true(kb_project(has_role(Obj,Role) during Action)),
+	assert_true(mongolog_call(project(has_role(Obj,Role) during Action))),
 	assert_true(has_role(Obj,Role) during Action).
 
 test('has_transition') :-
 	rdf_equal(test:'TestColor_0',Q),
 	% TODO: assert a description of the transistion, then state that
 	%          the episode satisfies this description.
-	assert_true(kb_project(has_region(Q,test:'TEST_GREEN') until 300)),
-	assert_true(kb_project(has_region(Q,test:'TEST_RED') since 300)),
+	assert_true(mongolog_call(project(has_region(Q,test:'TEST_GREEN') until 300))),
+	assert_true(mongolog_call(project(has_region(Q,test:'TEST_RED') since 300))),
 	assert_true(has_region(Q,test:'TEST_RED')),
 	assert_false(has_region(Q,test:'TEST_GREEN')),
 	assert_true(has_region(Q,test:'TEST_GREEN') during [200,250]).
 
 test('action_succeeded') :-
+	universal_scope(Scope),
 	test_action(Action),
 	assert_false(action_succeeded(Action)),
-	assert_true(kb_project(action_succeeded(Action))),
+	assert_true(mongolog_call(project(action_succeeded(Action)), [scope(Scope)])),
 	assert_true(action_succeeded(Action)).
 
 test('is_masterful') :-
+	universal_scope(Scope),
 	test_episode(Episode),
 	%%
-	kb_call(new_iri(Masterful)),
-	kb_project(has_type(Masterful,test:'Masterful')),
+	mongolog_call(new_iri(Masterful)),
+	mongolog_call(project(has_type(Masterful,test:'Masterful')), [scope(Scope)]),
 	assert_true(ground(Masterful)),
 	assert_true(is_diagnosis(Masterful)),
 	%%
 	assert_false(satisfies(Episode,Masterful)),
-	assert_true(kb_project(satisfies(Episode,Masterful))),
+	assert_true(mongolog_call(project(satisfies(Episode,Masterful)), [scope(Scope)])),
 	assert_true(satisfies(Episode,Masterful)).
 
-%tests(mem_predicate_add_role) :-
-  %test_action(TskNode,_,_),
-  %mem_event_add_role(TskNode, knowrob:'Reasoner'),
-  %mem_event_add_classification(TskNode,
-      %tests:'predicate1', knowrob:'Reasoner').
-
-%tests(mem_predicate_assignment) :-
-  %test_action(TskNode,_,_),
-  %mem_event_add_assignment(TskNode,
-      %tests:'predicate1_Argument1',tests:'Bowl_0'),
-  %mem_event_add_assignment(TskNode,
-      %tests:'predicate1_Argument2',tests:'Artifact_0'),
-  %%%
-  %mem_event_assignment(TskNode,
-      %tests:'predicate1_Argument1',tests:'Bowl_0'),
-  %mem_event_assignment(TskNode,
-      %tests:'predicate1_Argument2',tests:'Artifact_0').
-
-:- end_rdf_tests(model_NEEM).
+:- end_mongolog_tests(mongolog_neems).
