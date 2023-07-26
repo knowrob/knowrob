6a237f834f71cdcdf0aff355b5ad64de41d44efc
Daniel Beßler <danielb@uni-bremen.de>
2023-07-26 17:44:35 +0200

    added comment

--- include/knowrob/backend/KnowledgeGraph.h
+++ include/knowrob/backend/KnowledgeGraph.h
@@ -41,16 +41,36 @@ namespace knowrob {
 
         ~KnowledgeGraph();
 
-        void setThreadPool(const std::shared_ptr<ThreadPool> &threadPool);
-
+        /**
+         * Initialize this KG from a property tree.
+         * Note that each KG implementation has a different set of properties used
+         * to configure it.
+         * @param config a property tree.
+         * @return true on success
+         */
         virtual bool loadConfiguration(const boost::property_tree::ptree &config) = 0;
 
+        /**
+         * @return the vocabulary of this KG.
+         */
         const auto& vocabulary() const { return vocabulary_; }
 
+        /**
+         * @param iri a RDF resource IRI
+         * @return true if the KG contains a statement involving the resource IRI
+         */
         bool isDefinedResource(const std::string_view &iri);
 
+        /**
+         * @param iri a RDF property IRI
+         * @return true if the KG contains a statement involving the property IRI
+         */
         bool isDefinedProperty(const std::string_view &iri);
 
+        /**
+         * @param iri a RDF class IRI
+         * @return true if the KG contains a statement involving the class IRI
+         */
         bool isDefinedClass(const std::string_view &iri);
 
         /**
@@ -68,21 +88,49 @@ namespace knowrob {
         bool loadTriples(const std::string_view &uriString, TripleFormat format)
         { return loadTriples(uriString, format, std::nullopt); }
 
+        /**
+         * Add an assertion to this KG.
+         * @param tripleData data representing an atomic proposition.
+         * @return true on success
+         */
         virtual bool insert(const TripleData &tripleData) = 0;
 
+        /**
+         * Add assertions to this KG.
+         * @param tripleData data representing atomic propositions.
+         * @return true on success
+         */
         virtual bool insert(const std::vector<TripleData> &tripleData) = 0;
 
+        /**
+         * Delete all matching statements from this KG.
+         * @param tripleExpression an expression used to match statements in the KG.
+         */
         virtual void removeAll(const semweb::TripleExpression &tripleExpression) = 0;
 
+        /**
+         * Delete the first matching statement from this KG.
+         * @param tripleExpression an expression used to match statements in the KG.
+         */
         virtual void removeOne(const semweb::TripleExpression &tripleExpression) = 0;
 
         /**
-         * Find instantiations of a literal in the knowledge graph.
+         * Submits a graph query to this knowledge graph.
+         * The query is evaluated concurrently, and evaluation may still be active
+         * when this function returns.
+         * The function returns a stream of solutions, the end of the stream is indicated
+         * by an EOS message.
          * @param query a graph query
          * @return a stream with answers to the query
          */
         AnswerBufferPtr submitQuery(const GraphQueryPtr &query);
 
+        /**
+         * Evaluates a query and may block until evaluation completed.
+         * All results will be written into the provided stream object.
+         * @param query a query.
+         * @param resultStream a stream of answers.
+         */
         virtual void evaluateQuery(const GraphQueryPtr &query, AnswerBufferPtr &resultStream) = 0;
 
         /**
@@ -103,7 +151,7 @@ namespace knowrob {
         static std::string getNameFromURI(const std::string &uriString);
 
         /**
-         * Get a version string from an ontology URI.
+         * Extract a version string from an ontology URI.
          * In case the URI points to a local file, the modification time of the file
          * is used as version.
          * For other URIs it is attempted to extract version information from the URI string,
@@ -114,6 +162,13 @@ namespace knowrob {
         static std::string getVersionFromURI(const std::string &uriString);
 
         static bool isVersionString(const std::string &versionString);
+
+        /**
+         * Assign thread pool to be used during query evaluation.
+         * This is optional, and if not called a default one will be used.
+         * @param threadPool a thread pool.
+         */
+        void setThreadPool(const std::shared_ptr<ThreadPool> &threadPool);
 
     protected:
         std::shared_ptr<ThreadPool> threadPool_;
