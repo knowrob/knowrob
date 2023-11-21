c4427018a0baf7666e48d6cdef2fe9a4ef4d49d7
Daniel Beßler <danielb@cs.uni-bremen.de>
2023-11-21 15:29:48 +0100

    added notes

--- tests/KnowledgeBaseTest.cpp
+++ tests/KnowledgeBaseTest.cpp
@@ -12,6 +12,17 @@ protected:
     // add some helper functions below...
 };
 
+// TODO:
+// here main interface of KnoweldgeBase should be tested, in particular:
+// - submitQuery*
+// - insert*
+//
+// what is needed:
+// - setup database backend for testing, insert tmp data on which queries can be evaluated
+// - generate an adhoc configuration for KnowledgeBase. currently this must be a boost property tree
+// - use some dummy reasoner with fixed input-output mapping that can be tested easily
+//
+
 TEST_F(KnowledgeBaseTest, ThisFails)
 {
     EXPECT_EQ(1, 0);
