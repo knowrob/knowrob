0eb24038e8184d760a4bef029aa2cbf9f4df9af1
Daniel Beßler <danielb@uni-bremen.de>
2023-11-22 18:11:33 +0100

    testcases for EDB queries

--- tests/KnowledgeBaseTest.cpp
+++ tests/KnowledgeBaseTest.cpp
@@ -37,6 +37,19 @@ static std::vector<SubstitutionPtr> lookupAll(const std::string &queryString) {
 	return out;
 }
 
+static bool containsAnswer(const std::vector<SubstitutionPtr> &answers, const std::string &key, const TermPtr &value) {
+	Variable v_key(key);
+	for(auto x: answers) {
+		if(x->contains(v_key)) {
+			auto actual = x->get(v_key);
+			if(*value == *actual) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 TEST_F(KnowledgeBaseTest, undefinedNamespace) {
 	EXPECT_THROW(lookupAll("undefined:hasSibling(swrl_test:Fred, X)"), QueryError);
 }
@@ -48,4 +61,30 @@ TEST_F(KnowledgeBaseTest, atomic_EDB) {
 	EXPECT_EQ(lookupAll("swrl_test:hasSibling(swrl_test:Lea, X)").size(), 0);
 	EXPECT_EQ(lookupAll("swrl_test:hasSibling(X, swrl_test:Ernest)").size(), 1);
 	EXPECT_EQ(lookupAll("swrl_test:hasSibling(swrl_test:Fred, swrl_test:Ernest)").size(), 1);
+}
+
+TEST_F(KnowledgeBaseTest, conjunctive_EDB) {
+	const auto queryString = "swrl_test:hasSibling(swrl_test:Fred,X) , swrl_test:hasNumber(X,Num)";
+	EXPECT_EQ(lookupAll(queryString).size(), 1);
+	EXPECT_EQ(*lookupAll(queryString)[0]->get("Num"), StringTerm("123456"));
+}
+
+TEST_F(KnowledgeBaseTest, disjunctive_EDB) {
+	const auto queryString = "swrl_test:hasSibling(swrl_test:Fred,X) ; swrl_test:hasAncestor(swrl_test:Fred,X)";
+	EXPECT_EQ(lookupAll(queryString).size(), 2);
+	EXPECT_TRUE(containsAnswer(lookupAll(queryString), "X", QueryParser::parseConstant("swrl_test:Ernest")));
+	EXPECT_TRUE(containsAnswer(lookupAll(queryString), "X", QueryParser::parseConstant("swrl_test:Rex")));
+}
+
+TEST_F(KnowledgeBaseTest, negated_EDB) {
+	const auto queryString = "~swrl_test:hasSibling(swrl_test:Lea,X)";
+	EXPECT_EQ(lookupAll(queryString).size(), 1);
+	EXPECT_TRUE(lookupAll(queryString)[0]->empty());
+}
+
+TEST_F(KnowledgeBaseTest, complex_EDB) {
+	const auto queryString = "(swrl_test:hasSibling(swrl_test:Fred,X) ; "\
+							 "swrl_test:hasAncestor(swrl_test:Fred,X)) , swrl_test:hasSibling(X,Y)";
+	EXPECT_EQ(lookupAll(queryString).size(), 1);
+	EXPECT_TRUE(containsAnswer(lookupAll(queryString), "X", QueryParser::parseConstant("swrl_test:Ernest")));
 }
