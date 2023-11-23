1412b7409296cb3705907b4d009441766970ca1f
Daniel Beßler <danielb@cs.uni-bremen.de>
2023-11-23 16:08:28 +0100

    code formatting

--- include/knowrob/queries/NegationStage.h
+++ include/knowrob/queries/NegationStage.h
@@ -9,22 +9,25 @@
 #include "knowrob/reasoner/ReasonerManager.h"
 
 namespace knowrob {
+	/**
+	 * A stage that evaluates a set of negations which are considered
+	 * in conjunction.
+	 */
+	class NegationStage : public AnswerBroadcaster {
+	public:
+		NegationStage(const std::shared_ptr<KnowledgeGraph> &kg,
+					  const std::shared_ptr<ReasonerManager> &reasonerManager,
+					  const std::vector<RDFLiteralPtr> &negativeLiterals);
 
-    class NegationStage : public AnswerBroadcaster {
-    public:
-        NegationStage(const std::shared_ptr<KnowledgeGraph> &kg,
-                      const std::shared_ptr<ReasonerManager> &reasonerManager,
-                      const std::vector<RDFLiteralPtr> &negativeLiterals);
+		bool succeeds(const AnswerPtr &answer);
 
-        bool succeeds(const AnswerPtr &answer);
+	protected:
+		std::shared_ptr<KnowledgeGraph> kg_;
+		std::shared_ptr<ReasonerManager> reasonerManager_;
+		const std::vector<RDFLiteralPtr> negativeLiterals_;
 
-    protected:
-        std::shared_ptr<KnowledgeGraph> kg_;
-        std::shared_ptr<ReasonerManager> reasonerManager_;
-        const std::vector<RDFLiteralPtr> negativeLiterals_;
-
-        void pushToBroadcast(const AnswerPtr &msg) override;
-    };
+		void pushToBroadcast(const AnswerPtr &msg) override;
+	};
 
 } // knowrob
 
