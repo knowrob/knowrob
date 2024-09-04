830f5900d5f9095e84c2e3c24b831fb1605f4b5d
mpomarlan <mpomarlan@yahoo.co.uk>
2024-09-04 12:15:27 +0200

    Updated to reflect upstream changes in dfl.dlquery

--- src/reasoner/soma_dfl/soma_dfl.py
+++ src/reasoner/soma_dfl/soma_dfl.py
@@ -1,6 +1,6 @@
 from knowrob import *
 
-import dfl.semrep as sr
+import dfl.dlquery as dl
 
 def _iriOrVariable(x):
     if x.isVariable():
@@ -10,6 +10,7 @@ def _iriOrVariable(x):
 class SOMADFLReasoner(GoalReasoner):
     def __init__(self):
         super().__init__()
+        self.reasoner = dl.DFLReasoner()
         self.hasDisposition = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#hasDisposition")
         self.isDispositionOf = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#isDispositionOf")
         self.hasPart = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#hasPart")
@@ -21,59 +22,61 @@ class SOMADFLReasoner(GoalReasoner):
         self.defineRelation(self.hasDisposition)
         self.defineRelation(self.hasPart)
         self.defineRelation(self.hasConstituent)
+        self.defineRelation(self.isDispositionOf)
+        self.defineRelation(self.isPartOf)
+        self.defineRelation(self.isConstituentOf)
+        self.defineRelation(self.useMatch)
         self.simpleGoals = {self.hasPart, self.hasConstituent, self.hasDisposition, self.isDispositionOf, self.isPartOf, self.isConstituentOf}
         self.inverseProperties = {self.isDispositionOf: self.hasDisposition, self.isPartOf: self.hasPart, self.isConstituentOf: self.hasConstituent}
-        self.fnMap = {self.hasDisposition: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, sr.dl.whatDispositionsDoesObjectHave, sr.dl.whatHasDisposition),
-                      self.hasPart: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, sr.dl.whatPartTypesDoesObjectHave, sr.dl.whatHasPartType),
-                      self.hasConstituent: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, sr.dl.whatConstituentsDoesObjectHave, sr.dl.whatHasConstituent)}
+        self.fnMap = {self.hasDisposition: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, self.reasoner.whatDispositionsDoesObjectHave, self.reasoner.whatHasDisposition),
+                      self.hasPart: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, self.reasoner.whatPartTypesDoesObjectHave, self.reasoner.whatHasPartType),
+                      self.hasConstituent: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, self.reasoner.whatConstituentsDoesObjectHave, self.reasoner.whatHasConstituent)}
         self.classes = set()
-        # TODO: UseMatch: task, instrument, object acted on
         
     def _evaluateSimpleGoal(self, p, s, o, bounding, fnOVar, fnSVar):
         if ((False, True) == bounding):
             todo = self._ensureIndividual2Classes(s)
             dispositions = set().union(*[fnOVar(str(c)) for c in todo])
-            _ = [goal.push(Bindings({o: IRIAtom(sr.dl.expandName(d))})) for d in dispositions]
+            _ = [goal.push(Bindings({o: IRIAtom(self.reasoner.expandName(d))})) for d in dispositions]
         else:
-            concepts = [sr.dl.expandName(c) for c in fnSVar(str(o))]
+            concepts = [self.reasoner.expandName(c) for c in fnSVar(str(o))]
             instances = set.union(*[self._ensureClass2Individuals(c) for c in concepts])
             if ((True, False) == bounding):
-                _ = [goal.push(Bindings({s: IRIAtom(sr.dl.expandName(i))})) for i in instances]
+                _ = [goal.push(Bindings({s: IRIAtom(self.reasoner.expandName(i))})) for i in instances]
             elif ((False, False) == bounding) and str(s) in instances:
                 goal.push(Bindings({}))
         return True
 
     def _ensureIndividual2Classes(self, entity):
         entity = str(entity)
-        entity = sr.dl.expandName(entity)
+        entity = self.reasoner.expandName(entity)
         retq = [entity]
         if entity not in self.classes:
             query_term = GraphSequence([GraphPattern(IRIAtom(entity), self.rdfType, Variable("z"))])
             retq = []
             self.storage().query(GraphQuery(query_term), lambda bindings : retq.append(bindings.get(Variable("z"))))
-            retq = list(set().union(*[sr.dl.whatSuperclasses(str(c)) for c in retq]))
+            retq = list(set().union(*[self.reasoner.whatSuperclasses(str(c)) for c in retq]))
         return retq
         
     def _ensureClass2Individuals(self, entity):
         entity = str(entity)
-        entity = sr.dl.expandName(entity)
+        entity = self.reasoner.expandName(entity)
         retq = [entity]
         if entity in self.classes:
             retq = set()
-            for sc in sr.dl.whatSubclasses(str(entity)):
+            for sc in self.reasoner.whatSubclasses(str(entity)):
                 query_term = GraphSequence([GraphPattern(Variable("z"), self.rdfType, IRIAtom(sc))])
                 self.storage().query(GraphQuery(query_term), lambda bindings : retq.add(str(bindings.get(Variable("z")))))
             retq = list(retq)
         return retq
                 
     def initializeReasoner(self, config: PropertyTree) -> bool:
-        # TODO: MAJOR: separate cache, namespace etc. for each instance of this class, right now it is only allowed to be a singleton
         # TODO: parse and use config.
         # Flags that would make sense:
         #     ontology and usematch files to use
         #     namespace prefixes
-        sr.initializeOntology()
-        self.classes = set([sr.dl.expandName(x) for x in sr.dl.whatSubclasses("owl:Thing")])
+        self.reasoner.initializeOntology()
+        self.classes = set([self.reasoner.expandName(x) for x in self.reasoner.whatSubclasses("owl:Thing")])
         return True
 
     def evaluate(self, goal: Goal) -> bool:
@@ -107,17 +110,17 @@ class SOMADFLReasoner(GoalReasoner):
                 raise ValueError("Must specify at least one of patient or instrument in a useMatch query")
             if ((False, True) == bounding):
                 todo = self._ensureIndividual2Classes(instrument)
-                classes = set().union(*[sr.dl.whatObjectsCanToolPerformTaskOn(str(task), c) for c in todo])
-                classes = [sr.dl.expandName(c) for c in classes]
+                classes = set().union(*[self.reasoner.whatObjectsCanToolPerformTaskOn(str(task), c) for c in todo])
+                classes = [self.reasoner.expandName(c) for c in classes]
                 instances = set().union(*[self._ensureClass2Individuals(c) for c in classes])
-                _ = [goal.push(Bindings({patient: IRIAtom(sr.dl.expandName(i))})) for i in instances]
+                _ = [goal.push(Bindings({patient: IRIAtom(self.reasoner.expandName(i))})) for i in instances]
             else:
                 todo = self._ensureIndividual2Classes(patient)
-                classes = set().union(*[sr.dl.whatToolsCanPerformTaskOnObject(str(task), c) for c in todo])
-                classes = [sr.dl.expandName(c) for c in classes]
+                classes = set().union(*[self.reasoner.whatToolsCanPerformTaskOnObject(str(task), c) for c in todo])
+                classes = [self.reasoner.expandName(c) for c in classes]
                 instances = set().union(*[self._ensureClass2Individuals(c) for c in classes])
                 if (True, False) == bounding:
-                    _ = [goal.push(Bindings({instrument: IRIAtom(sr.dl.expandName(i))})) for i in instances]
+                    _ = [goal.push(Bindings({instrument: IRIAtom(self.reasoner.expandName(i))})) for i in instances]
                 elif ((False, False) == bounding) and (str(instrument) in instances):
                     goal.push(Bindings({}))
         return True # False: reasoning error
