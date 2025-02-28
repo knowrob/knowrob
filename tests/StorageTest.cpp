40a558acdc7c27268667cb324f425f68142a0b2d
Daniel Beßler <danielb@uni-bremen.de>
2025-02-28 11:50:45 +0100

    added a test case for storage queries containing triples with duplicate variable

--- tests/StorageTest.cpp
+++ tests/StorageTest.cpp
@@ -165,6 +165,10 @@ TYPED_TEST(StorageTest, Assert_a_b_c) {
 	EXPECT_EQ(TEST_LOOKUP(parse("triple(swrl_test:x, swrl_test:b, C)")).size(), 0);
 }
 
+TYPED_TEST(StorageTest, QueryDuplicateVars) {
+	EXPECT_EQ(TEST_LOOKUP(parse("triple(?x, swrl_test:b, ?x)")).size(), 0);
+}
+
 TYPED_TEST(StorageTest, TripleWithOrigin) {
 	TripleCopy data_cbd(swrl_test_"c", swrl_test_"b", swrl_test_"d");
 	EXPECT_EQ(TEST_LOOKUP(data_cbd).size(), 0);
